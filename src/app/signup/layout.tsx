032ac8afc35a648802dfe1fbe86c77c36f93b658
wiyco <72733890+wiyco@users.noreply.github.com>
2023-06-26 03:27:09 +0900

    Add layout of `Sign up`

---
+++ src/app/signup/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Sign up | IMAP",
+  description: "This is a sign up page. You can create an account from this page.",
+};
+
+export default function SignupLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
