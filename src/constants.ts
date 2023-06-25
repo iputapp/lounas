92749519cc845636c154a4508b27516af0a9f55f
wiyco <72733890+wiyco@users.noreply.github.com>
2023-06-26 03:25:45 +0900

    Add `constants` file

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * @constant {number} STUDENT_ID_LENGTH
+ *
+ * Length of `student ID`
+ */
+export const STUDENT_ID_LENGTH = 6;
+
+/**
+ * @constant {RegExp} STUDENT_ID_REGEX
+ *
+ * Regex of `student ID`
+ */
+export const STUDENT_ID_REGEX = /^[0-9]{6}$/;
+
+/**
+ * @constant {string} EMAIL_DOMAIN
+ *
+ * Domain of `email`
+ */
+export const EMAIL_DOMAIN = "tks.iput.ac.jp";
