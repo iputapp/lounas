49a6153c08e8c34742329cde3fd284b72bfe47f3
wiyco <72733890+wiyco@users.noreply.github.com>
2023-06-26 02:24:13 +0900

    Update environment settings (#2)
    - Add `sassOptions` to use `@use` in `.sass`.
    - Delete `./src/pages/**/*` from `tailwind.config.js`.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+/**
+ * @summary
+ * https://nextjs.org/docs/app/building-your-application/styling/sass
+ */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+  },
+};
 
 module.exports = nextConfig;
