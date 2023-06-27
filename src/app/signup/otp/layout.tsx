eca44820013606cfb3a5d4cb6517f912f71d6f38
wiyco <72733890+wiyco@users.noreply.github.com>
2023-06-28 02:01:29 +0900

    Add page to confirm sign up (#8)
    * Add constant variables
    
    - VERIFY_DIGITS
    
    * Add `layout` of `signup/otp`
    
    * Add `signup/otp` page
    
    - page
    - style

---
+++ src/app/signup/otp/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Sign up - confirm | IMAP",
+  description:
+    "This is a confirm page of sign up. You need to confirm your sign up with 6 digits on email.",
+};
+
+export default function OtpLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
