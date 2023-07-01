166e5c773cdfc3ae650ae8af0b53de4b881d317c
wiyco <72733890+wiyco@users.noreply.github.com>
2023-07-02 03:55:08 +0900

    Fix warning on supabase auth
    - [Details](https://supabase.com/docs/reference/javascript/initializing)

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+/** @see {@link https://supabase.com/docs/reference/javascript/initializing} */
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
 );
