a45fb3e7b30df5cb2fa9038b08703fde8e33de84
wiyco <72733890+wiyco@users.noreply.github.com>
2023-07-01 19:26:47 +0900

    Fixup environment files (#9)
    - Comments.
    - VS Code settings.
    - Add packages.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/**
- * @summary
- * https://nextjs.org/docs/app/building-your-application/styling/sass
- */
+/** @see {@link https://nextjs.org/docs/app/building-your-application/styling/sass} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /** @summary https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions#convention */
+  /** @see {@link https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions#convention} */
   experimental: {
     serverActions: true,
   },
