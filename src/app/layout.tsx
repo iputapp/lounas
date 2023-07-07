a8454b10e2eaacf9da53fbf9326dff65c3a3fb70
wiyco <72733890+wiyco@users.noreply.github.com>
2023-07-07 17:05:17 +0900

    Update `Layout tree` to more flexible (#27)
    * Update `RootLayout` to more flexible
    
    * Update `SignupLayout` with `RootLayout` changes
    
    * Remove duplicate styles of `signup/template`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>
-        <header></header>
-        {children}
-        <footer></footer>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
