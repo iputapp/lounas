46d070c1e5af04910a4e75ce13b143706727c42c
kou-nagamine <136679628+kou-nagamine@users.noreply.github.com>
2023-08-18 22:48:27 +0900

    Revert "circle"
    This reverts commit 74f298e9204c08a5c10716423bebc18bbf98ec88.

--- src/app/test/component/navigations/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Test navigation | IMAP",
-  description: "PROJECT PIPLUP",
-};
-
-export default function TestDialogInfoLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- src/app/test/component/navigations/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BottomSelector } from "@/components/FourPoti/Potibox";
-
-export default function Test() {
-  return (
-    <div>
-      <BottomSelector />
-    </div>
-  );
-}
