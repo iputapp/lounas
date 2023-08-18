b44b2cbe582be34d5ddfb370b43c739c37f9d5b8
HIMA-HIMAZIN <118464042+HIMA-HIMAZIN@users.noreply.github.com>
2023-08-19 00:47:42 +0900

    test/backbutton/layoutの追加

---
+++ src/app/test/backbutton/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Test BackButton | IMAP",
+  description: "PROJECT PIPLUP",
+};
+
+export default function TestBackButtonLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
