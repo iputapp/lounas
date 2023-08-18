abe3483150724a135c8a26ee9d671834d4928b77
HIMA-HIMAZIN <118464042+HIMA-HIMAZIN@users.noreply.github.com>
2023-08-19 04:45:52 +0900

    test/decieded-button/layoutの追加
    ファイル名変更予定あり

---
+++ src/app/test/decided-button/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Test DecidedButton | IMAP",
+  description: "PROJECT PIPLUP",
+};
+
+export default function TestDecidedButtonLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
