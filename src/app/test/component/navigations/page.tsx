dd3e970787e18c8568f14af85a4df19e5f58e817
kou-nagamine <136679628+kou-nagamine@users.noreply.github.com>
2023-08-18 22:18:04 +0900

    circle
    create circle as component

--- src/app/test/component/navigations/page.tsx
+++ src/app/test/component/navigations/page.tsx
@@ -1,9 +1,19 @@
-import { BottomSelector } from "@/components/FourPoti/Potibox";
+import { Circle } from "@/components/FourPoti/Circle";
 
 export default function Test() {
   return (
-    <div>
-      <BottomSelector />
+    <div style={{ position: "relative", height: "900px", width: "900px" }}>
+      <Circle
+        size={300}
+        position={{ x: 30, y: 0 }}
+        color1="red"
+        color2="blue"
+        label="950円~"
+        angle={90}
+      />
+      <Circle size={100} position={{ x: 320, y: 0 }} color1="blue" color2="red" label="750~950円" />
+      <Circle size={400} position={{ x: 0, y: 320 }} color1="pink" color2="red" label="~750円" />
+      <Circle size={50} position={{ x: 430, y: 350 }} color1="green" color2="blue" label="skip" />
     </div>
   );
 }
--- src/components/FourPoti/Circle.tsx
+++ src/components/FourPoti/Circle.tsx
@@ -3,17 +3,21 @@ import React from "react";
 interface CircleProps {
   size: number; // 円の大きさ
   position: { x: number; y: number }; // 円の位置
-  color: string; // 円の色
+  color1: string; // 円の色
+  color2: string;
   label?: string; // 円のラベル（オプション）
+  angle?: number; // 円のグラデーション角度（オプション）
 }
 
-const Circle: React.FC<CircleProps> = ({ size, position, color, label }) => {
+const Circle = (props: CircleProps) => {
+  const { size, position, color1, color2, label, angle = 0 } = props;
+
   return (
     <div
       style={{
         width: `${size}px`,
         height: `${size}px`,
-        backgroundColor: color,
+        background: `linear-gradient(${angle}deg, ${color1}, ${color2})`, // backgroundプロパティを使用
         borderRadius: "50%",
         position: "absolute",
         left: `${position.x}px`,
@@ -21,7 +25,6 @@ const Circle: React.FC<CircleProps> = ({ size, position, color, label }) => {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        color: "white",
         fontWeight: "bold",
       }}
     >
@@ -30,4 +33,4 @@ const Circle: React.FC<CircleProps> = ({ size, position, color, label }) => {
   );
 };
 
-export default Circle;
+export { Circle };
