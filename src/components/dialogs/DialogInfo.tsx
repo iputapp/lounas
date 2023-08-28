eafb32deb43776a8e6a370b73e87ca0ac4717b8e
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-28 13:51:16 +0900

    テスト用テキストの削除

--- src/components/dialogs/DialogInfo.tsx
+++ src/components/dialogs/DialogInfo.tsx
@@ -1,5 +1,5 @@
 import Cancel from "@icons/cancel.svg";
-import { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 import styles from "@/styles/components/dialogs/dialoginfo.module.scss";
 
@@ -13,17 +13,15 @@ type DialogInfoProps = {
 export function DialogInfo({ header, children, isOpen, setIsOpen }: DialogInfoProps) {
   return (
     <div className={styles.container + `${isOpen ? " visible" : " invisible"}`}>
-      <div>
-        <div className={styles.header}>
-          <button onClick={() => setIsOpen(!isOpen)}>
-            <Cancel />
-          </button>
-          <h1>
-            <span>{header}</span>
-          </h1>
-        </div>
-        <div className={styles.main}>{children}</div>
+      <div className={styles.header}>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <Cancel />
+        </button>
+        <h1>
+          <span>{header}</span>
+        </h1>
       </div>
+      <div className={styles.main}>{children}</div>
     </div>
   );
 }
