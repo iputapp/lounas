c92151368159ffe0f1607b7f6657c84a7e82b110
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-29 22:54:13 +0900

    テスト確認

---
+++ src/app/test/decided-button/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Button } from "@/components/buttons/DecidedButton";
+
+export default function TestDecidedButton() {
+  return <Button id="" text="行く！！！！" color="god" />;
+}
