f332b143ae91b6d3041f6900286ab6e270ebb4b0
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-30 01:05:01 +0900

    削除

--- src/app/test/decided-button/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Button } from "@/components/buttons/DecidedButton";
-
-export default function TestDecidedButton() {
-  return <Button id="" text="行く！！！！" color="red" />;
-}
