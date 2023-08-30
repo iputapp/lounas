73058249e0c44776107eb25ba01d0f292d54e39c
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-31 04:19:17 +0900

    layoutファイル削除

--- src/app/test/decided-button/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Test DecidedButton | IMAP",
-  description: "PROJECT PIPLUP",
-};
-
-export default function TestDecidedButtonLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
