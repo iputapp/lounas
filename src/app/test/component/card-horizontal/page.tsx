b78c6716f41bf530bac45f5ee23db8869ed8edc9
wiyco <72733890+wiyco@users.noreply.github.com>
2023-08-30 17:38:32 +0900

    Update page.tsx

--- src/app/test/component/card-horizontal/page.tsx
+++ src/app/test/component/card-horizontal/page.tsx
@@ -5,9 +5,17 @@ import { PaymentJugde } from "@/components/paymentJugde/PaymentJugde";
 
 export default function Test() {
   const paymentMethods = [
-    { payment: "現金", accepted: true },
-    { payment: "PayPay", accepted: false },
-    { payment: "交通系IC", accepted: false },
+    [
+      { payment: "現金", accepted: true },
+      { payment: "PayPay", accepted: false },
+      { payment: "交通系IC", accepted: false },
+    ],
+    [
+      { payment: "現金", accepted: true },
+      { payment: "PayPay", accepted: true },
+      { payment: "Rakuten Pay", accepted: false },
+      { payment: "交通系IC", accepted: true },
+    ],
   ];
 
   return (
@@ -16,25 +24,25 @@ export default function Test() {
         title="赤味噌油そば"
         image="/test/oilnoodle.jpg"
         tag={1}
-        description={<PaymentJugde paymentMethods={paymentMethods} />}
+        description={<PaymentJugde paymentMethods={paymentMethods[0]} />}
       />
       <CardHorizontal
         title="赤味噌油そば"
         image="/test/oilnoodle.jpg"
         tag={2}
-        description={<PaymentJugde paymentMethods={paymentMethods} />}
+        description={<PaymentJugde paymentMethods={paymentMethods[1]} />}
       />
       <CardHorizontal
         title="赤味噌油そば"
         image="/test/oilnoodle.jpg"
         tag={3}
-        description={<PaymentJugde paymentMethods={paymentMethods} />}
+        description={<PaymentJugde paymentMethods={paymentMethods[0]} />}
       />
       <CardHorizontal
         title="赤味噌油そば"
         image="/test/oilnoodle.jpg"
         tag={4}
-        description={<PaymentJugde paymentMethods={paymentMethods} />}
+        description={<PaymentJugde paymentMethods={paymentMethods[1]} />}
       />
     </div>
   );
