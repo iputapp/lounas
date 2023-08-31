b9b0b392bb6a7257e2dfa15adde6869768578cec
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-31 22:08:56 +0900

    scssのtextカラーの変更

--- src/app/test/privacy-screen/page.tsx
+++ src/app/test/privacy-screen/page.tsx
@@ -8,12 +8,12 @@ export default function Privacy() {
   return (
     <main className="main">
       <div className="privacy">
-        <h1 className="title">プライバシー</h1>
+        <h1>プライバシー</h1>
         <p>
           このアプリではユーザーがより便利にお使いいただける用に情報提供の協力をお願いしています。
         </p>
       </div>
-      <div className="dialiginfo"></div>
+      <div className="dialiginfo">私たちが考えるプライバシーについて詳しく知る</div>
       <div className="animation">
         <PrivacyPlayer />
       </div>
