9819552a342921dff28262d3801776bf9e382628
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-31 21:17:06 +0900

    div要素追加

--- src/app/test/privacy-screen/page.tsx
+++ src/app/test/privacy-screen/page.tsx
@@ -5,11 +5,21 @@ import { PrivacyPlayer } from "@/components/lottie/Privacy";
 export default function Privacy() {
   return (
     <main className="main">
+      <div className="privacy">
+        <h1 className="title">プライバシー</h1>
+        <p>
+          このアプリではユーザーがより便利にお使いいただける用に情報提供の協力をお願いしています。
+        </p>
+      </div>
+      <div className="dialiginfo">
+        <p>詳細はこちら</p>
+      </div>
       <div className="animation">
         <PrivacyPlayer />
       </div>
-      <h1>トラッキングを許可しますか？</h1>
-      <div></div>
+      <div className="choice">
+        <h1>トラッキングを許可しますか？</h1>
+      </div>
     </main>
   );
 }
