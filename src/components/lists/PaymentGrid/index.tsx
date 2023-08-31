6cc96cbf3ff1cc9fd34ecae079e5e83c893424fa
wiyco <72733890+wiyco@users.noreply.github.com>
2023-08-31 23:22:41 +0900

    Add `PaymentGrid` component

---
+++ src/components/lists/PaymentGrid/index.tsx
@@ -0,0 +1,28 @@
+import Cancel from "@icons/cancel.svg";
+import Check from "@icons/check.svg";
+
+type PaymentGridProps = {
+  payment: string;
+  accepted: boolean;
+};
+
+export function PaymentGrid({ payments }: { payments: PaymentGridProps[] }) {
+  return (
+    <ul className="grid grid-cols-2 gap-x-6 gap-y-2">
+      {payments.map((item) => (
+        <li key={item.payment} className="grid grid-cols-4 justify-items-center gap-1 text-sm">
+          <span className="col-span-3">{item.payment}</span>
+          {item.accepted ? (
+            <span className="text-green-600">
+              <Check />
+            </span>
+          ) : (
+            <span className="text-red-600">
+              <Cancel />
+            </span>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+}
