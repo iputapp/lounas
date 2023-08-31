4b03cbdf6dfdbc34c1f888729d741176f7f9d5f6
wiyco <72733890+wiyco@users.noreply.github.com>
2023-08-31 16:48:23 +0900

    Add `expandable-panel` component

---
+++ src/components/layouts/ExpandablePanel/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Cancel from "@icons/cancel.svg";
+import Expand from "@icons/expand.svg";
+import { useState } from "react";
+
+import styles from "./styles.module.scss";
+
+type ExpandablePanelProps = {
+  children: React.ReactNode;
+  expandChildren?: React.ReactNode;
+};
+
+export function ExpandablePanel({ children, expandChildren }: ExpandablePanelProps) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <div className={`${expanded && styles.backdrop}`}>
+      <div className={`${styles.container} ${expanded && styles.expanded}`}>
+        <div className={styles.content}>
+          {children}
+          {expanded && <div className={styles.content}>{expandChildren}</div>}
+        </div>
+        <button className={styles.icon} onClick={() => setExpanded(!expanded)}>
+          <span>{expanded ? <Cancel /> : <Expand />}</span>
+        </button>
+      </div>
+    </div>
+  );
+}
