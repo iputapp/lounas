00a18d5886dce9753ede7e0fbbd51599089cf8c6
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-31 22:43:18 +0900

    Rectbuttonコンポーネントの追加

--- src/app/test/privacy-screen/page.tsx
+++ src/app/test/privacy-screen/page.tsx
@@ -2,9 +2,13 @@
 
 import "./page.scss";
 
+import { RectButton } from "@/components/buttons/RectButton";
 import { PrivacyPlayer } from "@/components/lottie/Privacy";
 
 export default function Privacy() {
+  const onClick = () => {
+    console.log("button clicked");
+  };
   return (
     <main className="main">
       <div className="privacy">
@@ -19,6 +23,10 @@ export default function Privacy() {
       </div>
       <div className="choice">
         <h1>トラッキングを許可しますか？</h1>
+        <div className="button">
+          <RectButton text="拒否する" color="red" onClick={onClick}></RectButton>
+          <RectButton text="許可する" color="blue" onClick={onClick}></RectButton>
+        </div>
       </div>
     </main>
   );
