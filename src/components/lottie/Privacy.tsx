aa311681a1c52170be5dc5d62874712c41eeefcc
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-08-31 20:44:14 +0900

    関数名の変更

--- src/components/lottie/Privacy.tsx
+++ src/components/lottie/Privacy.tsx
@@ -2,6 +2,6 @@
 
 import { Player } from "@lottiefiles/react-lottie-player";
 
-export function LoadingPlayer() {
+export function PrivacyPlayer() {
   return <Player src="/lottie/privacy-animation.json" autoplay loop />;
 }
