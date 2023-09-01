3fd81368211bf9aceed0e7da0162f16cbc1b6cbf
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-01 15:20:32 +0900

    test用のpage追加

---
+++ src/app/test/footer/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Footer } from "@/components/navigations/Footer";
+
+export default function Test() {
+  return (
+    <main className="footer">
+      <Footer />
+    </main>
+  );
+}
