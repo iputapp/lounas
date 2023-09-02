d7cb3af8c0f59cd1f3068d7fddaba4a0ee49dde8
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-03 07:22:36 +0900

    Review commit

---
+++ src/components/buttons/BorderedRoundedButton/index.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import styles from "./styles.module.scss";
+
+type BorderedRoundedButtonProps = {
+  title: string;
+  fontSize?:
+    | "text-xs"
+    | "text-sm"
+    | "text-base"
+    | "text-lg"
+    | "text-xl"
+    | "text-2xl"
+    | "text-3xl"
+    | "text-4xl"
+    | "text-5xl"
+    | "text-6xl"
+    | "text-7xl"
+    | "text-8xl"
+    | "text-9xl";
+  onClick: () => void;
+};
+
+export function BorderedRoundedButton({ title, fontSize, onClick }: BorderedRoundedButtonProps) {
+  return (
+    <button className={`${styles.container} ${fontSize}`} onClick={onClick}>
+      <div className={styles.inner}>
+        <span className={styles.title}>{title}</span>
+      </div>
+    </button>
+  );
+}
