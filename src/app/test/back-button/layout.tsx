1918443c68c81cb1bdfa8a0945fd5e47fe154a73
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-02 15:06:32 +0900

    Delete layout.tsx

--- src/app/test/back-button/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Test BackButton | IMAP",
-  description: "PROJECT PIPLUP",
-};
-
-export default function TestBackButtonLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
