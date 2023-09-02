67ea3b9452dbab58ee375ba3624f8011570ca32b
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-02 15:05:41 +0900

    Review commit

--- src/app/test/back-button/page.tsx
+++ src/app/test/back-button/page.tsx
@@ -1,7 +1,16 @@
-"use client";
-
 import { BackButton } from "@/components/buttons/BackButton";
+import { CardHorizontal } from "@/components/cards/CardHorizontal";
 
 export default function TestBackButton() {
-  return <BackButton />;
+  return (
+    <div className="grid w-full gap-5 px-1 py-5">
+      <div className="p-1">
+        <BackButton />
+      </div>
+      <div className="grid gap-5 p-3">
+        <CardHorizontal title="カード" image="/icons/media-image.svg" tag={1} />
+        <CardHorizontal title="カード" image="/icons/media-image.svg" tag={2} />
+      </div>
+    </div>
+  );
 }
--- src/components/buttons/BackButton/index.tsx
+++ src/components/buttons/BackButton/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Arrow from "@icons/nav-arrow-left.svg";
+import ArrowSemibold from "@icons/nav-arrow-left-semibold.svg";
 import { useRouter } from "next/navigation";
 
 import styles from "./styles.module.scss";
@@ -9,13 +9,11 @@ export function BackButton() {
   const router = useRouter();
 
   return (
-    <div className={styles.main}>
-      <button className={styles.button} type="button" onClick={() => router.back()}>
-        <Arrow />
-        <h1>
-          <span>戻る</span>
-        </h1>
-      </button>
-    </div>
+    <button className={styles.button} type="button" onClick={() => router.back()}>
+      <span>
+        <ArrowSemibold />
+      </span>
+      <span>戻る</span>
+    </button>
   );
 }
