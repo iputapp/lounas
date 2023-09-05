d7b58da191bf909359a820f693f0f74606ce3848
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-05 20:57:18 +0900

    signInページで使用される「このアプリについて」コンポーネントの作成

---
+++ src/components/dummy/ThisApp/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export function ThisApp() {
+  return (
+    <div>
+      <div className="header">
+        <div className="title"></div>
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
