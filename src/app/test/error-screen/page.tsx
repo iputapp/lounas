a2f4102750c59924a360bf2f0414a431148af151
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-05 20:02:31 +0900

    画像をlottieに変更

--- src/app/test/error-screen/page.tsx
+++ src/app/test/error-screen/page.tsx
@@ -1,12 +1,14 @@
 import "./page.scss";
 
-import Warnig from "@icons/warning.svg";
+import { ErrorPlayer } from "@/components/lottie/Error";
 
 export default function Test() {
   return (
     <div className="main">
       <h1 className="title">エラー</h1>
-      <Warnig className="icon" />
+      <div className="animation">
+        <ErrorPlayer />
+      </div>
       <p className="text">認証コード確認時に問題が発生しました</p>
       <h2 className="please">時間をおいて再度お試しください</h2>
     </div>
