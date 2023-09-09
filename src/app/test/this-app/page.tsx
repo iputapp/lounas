c35927d16292bac6b0270228339037031aa6bd3d
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-09 21:51:51 +0900

    signin画面の背景に使用される三角デザインの作成とコンポーネントの表示確認

---
+++ src/app/test/this-app/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import "./style.scss";
+
+import { BorderRoundButton } from "@/components/buttons/BorderRoundButton";
+import { Thisapp } from "@/components/dummy/ThisApp";
+
+export default function Test() {
+  const onClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <>
+      <div className="bg">
+        <span className="circle blue"></span>
+        <span className="circle cyan"></span>
+        <span className="circle green1"></span>
+        <span className="circle green2"></span>
+      </div>
+      <div className="main">
+        <div className="title">アカウント</div>
+        <div className="go">
+          <div className="text">IMAP会員へようこそ !</div>
+          <div className="button">
+            <BorderRoundButton title="Sign IN" fontSize="text-2xl" onClick={onClick} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <Thisapp>
+            <p className="m-5">
+              sだfksジャhddds堕hflksjdhファslkjdfはslkdjfはsldkjfはslkjfはslkdjfはsldkfjhさldkjfkだskファjhdlかsjfはlskjfhぁksjhファlksdjhfぁkfsdjfhlksじゃdfh亞lkdjfhさlkjdfhsl亞kdjfはslkdfjはslkふぁsjhfsヵjはsldkfjhさldkfjhさlfはfl亞skjdfはslkjfだ
+            </p>
+          </Thisapp>
+        </div>
+      </div>
+    </>
+  );
+}
