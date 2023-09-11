fb44d04ed2af0767c85aad969084d6d00f0807ef
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-11 16:32:55 +0900

    MUIのSkeletonを使用したCardを追加しました。

--- src/app/test/loading-page/page.tsx
+++ src/app/test/loading-page/page.tsx
@@ -1,9 +1,89 @@
 "use client";
 
+import CardHeader from "@mui/material/CardHeader";
+import Skeleton from "@mui/material/Skeleton";
 import * as React from "react";
 
 import { RectButton } from "@/components/buttons/RectButton";
 import { BorderTitle } from "@/components/headers/BorderTitle";
+
+function Media() {
+  return (
+    <>
+      <CardHeader
+        avatar={
+          <Skeleton
+            sx={{ bgcolor: "grey.400" }}
+            animation="wave"
+            variant="rounded"
+            width={50}
+            height={50}
+          />
+        }
+        title={
+          <Skeleton
+            sx={{ bgcolor: "grey.400" }}
+            animation="wave"
+            height={20}
+            width="50%"
+            style={{ marginBottom: 10 }}
+          />
+        }
+        subheader={
+          <Skeleton sx={{ bgcolor: "grey.400" }} animation="wave" height={20} width={200} />
+        }
+      />
+
+      <CardHeader
+        avatar={
+          <Skeleton
+            sx={{ bgcolor: "grey.400" }}
+            animation="wave"
+            variant="rounded"
+            width={50}
+            height={50}
+          />
+        }
+        title={
+          <Skeleton
+            sx={{ bgcolor: "grey.400" }}
+            animation="wave"
+            height={20}
+            width="50%"
+            style={{ marginBottom: 10 }}
+          />
+        }
+        subheader={
+          <Skeleton sx={{ bgcolor: "grey.400" }} animation="wave" height={20} width={200} />
+        }
+      />
+
+      <CardHeader
+        avatar={
+          <Skeleton
+            sx={{ bgcolor: "grey.400" }}
+            animation="wave"
+            variant="rounded"
+            width={50}
+            height={50}
+          />
+        }
+        title={
+          <Skeleton
+            sx={{ bgcolor: "grey.400" }}
+            animation="wave"
+            height={20}
+            width="50%"
+            style={{ marginBottom: 10 }}
+          />
+        }
+        subheader={
+          <Skeleton sx={{ bgcolor: "grey.400" }} animation="wave" height={20} width={200} />
+        }
+      />
+    </>
+  );
+}
 
 export default function LoadingPage() {
   const onClick = () => {
@@ -11,11 +91,13 @@ export default function LoadingPage() {
   };
 
   return (
-    <div className="container">
+    <div className="container grid gap-5 ">
       <div className="m-5 flex items-center justify-center text-center">
         <BorderTitle title="あなたの候補をもとに検索しています" />
       </div>
-      <div></div>
+      <div className="m-5 ">
+        <Media />
+      </div>
       <div className="flex items-center justify-center">
         <RectButton text="キャンセル" color="red" onClick={onClick} />
       </div>
