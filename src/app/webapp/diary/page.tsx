13470d9bb14891e5e3ac04684ae370ec386004dc
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-11 15:42:20 +0900

    文字と背景のぼかしを追加

--- src/app/webapp/diary/page.tsx
+++ src/app/webapp/diary/page.tsx
@@ -1,12 +1,26 @@
-import Diary from "@test/calendar-mock.webp";
+import "./style.scss";
+
+import Image from "next/image";
+import Calender from "public/test/calendar-mock.webp";
 
 import { BottomNavigationBar } from "@/components/navigations/BottomNavigationBar";
 
 export default function DiaryPage() {
   return (
     <>
-      <div>
-        <Diary />
+      <div className="diary">
+        <Image
+          className="img"
+          src={Calender}
+          alt="calender"
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+        />
+        <section>
+          <div className="cs">Coming Soon!</div>
+          <div className="development">現在開発中です。</div>
+          <div className="caveat">(訪問履歴は現在も記録されています)</div>
+        </section>
       </div>
       <BottomNavigationBar />
     </>
