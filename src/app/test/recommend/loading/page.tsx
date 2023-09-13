887b0a9aab24110e41219d2a97b067316efe3df5
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-13 16:44:15 +0900

    MUIの変更とBorderTitleの文字サイズ指定

--- src/app/test/recommend/loading/page.tsx
+++ src/app/test/recommend/loading/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import CardHeader from "@mui/material/CardHeader";
 import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 import * as React from "react";
 
 import { RectButton } from "@/components/buttons/RectButton";
@@ -9,79 +9,18 @@ import { BorderTitle } from "@/components/headers/BorderTitle";
 
 function Media() {
   return (
-    <>
-      <CardHeader
-        avatar={
-          <Skeleton
-            sx={{ bgcolor: "grey.500" }}
-            animation="wave"
-            variant="rounded"
-            width={30}
-            height={30}
-          />
-        }
-        title={
-          <Skeleton
-            sx={{ bgcolor: "grey.500" }}
-            animation="wave"
-            height={10}
-            width="20%"
-            style={{ marginBottom: 5 }}
-          />
-        }
-        subheader={
-          <Skeleton sx={{ bgcolor: "grey.500" }} animation="wave" height={10} width={100} />
-        }
-      />
-
-      <CardHeader
-        avatar={
-          <Skeleton
-            sx={{ bgcolor: "grey.500" }}
-            animation="wave"
-            variant="rounded"
-            width={40}
-            height={40}
-          />
-        }
-        title={
-          <Skeleton
-            sx={{ bgcolor: "grey.500" }}
-            animation="wave"
-            height={15}
-            width="40%"
-            style={{ marginBottom: 6 }}
-          />
-        }
-        subheader={
-          <Skeleton sx={{ bgcolor: "grey.500" }} animation="wave" height={15} width={150} />
-        }
-      />
-
-      <CardHeader
-        avatar={
-          <Skeleton
-            sx={{ bgcolor: "grey.500" }}
-            animation="wave"
-            variant="rounded"
-            width={50}
-            height={50}
-          />
-        }
-        title={
-          <Skeleton
-            sx={{ bgcolor: "grey.500" }}
-            animation="wave"
-            height={20}
-            width="50%"
-            style={{ marginBottom: 10 }}
-          />
-        }
-        subheader={
-          <Skeleton sx={{ bgcolor: "grey.500" }} animation="wave" height={20} width={200} />
-        }
-      />
-    </>
+    <div className="grid gap-5">
+      <Stack spacing={1}>
+        <Skeleton variant="rounded" width={200} height={100} sx={{ bgcolor: "grey.700" }} />
+        <Skeleton variant="rounded" width={200} height={10} sx={{ bgcolor: "grey.700" }} />
+        <Skeleton variant="rounded" width={100} height={10} sx={{ bgcolor: "grey.700" }} />
+      </Stack>
+      <Stack spacing={1}>
+        <Skeleton variant="rounded" width={200} height={100} sx={{ bgcolor: "grey.700" }} />
+        <Skeleton variant="rounded" width={200} height={10} sx={{ bgcolor: "grey.700" }} />
+        <Skeleton variant="rounded" width={100} height={10} sx={{ bgcolor: "grey.700" }} />
+      </Stack>
+    </div>
   );
 }
 
@@ -92,8 +31,8 @@ export default function LoadingPage() {
 
   return (
     <div className="container grid h-screen items-center justify-center">
-      <div className="mx-3 flex items-center justify-center text-center">
-        <BorderTitle title="あなたの候補をもとに検索しています" />
+      <div className="mx-5 flex items-center justify-center text-center">
+        <BorderTitle title="あなたの候補をもとに検索しています" fontSize="text-3xl" />
       </div>
       <div className="m-5 flex  flex-col items-center">
         <Media />
