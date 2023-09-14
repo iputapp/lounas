c2738ab22b0e3d3aad498499122308e03c85b208
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-14 22:02:18 +0900

    user画面で使用するフォルダとページの確保

---
+++ src/app/webapp/user/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+import styles from "./layout.module.scss";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className={styles.bg}>
+        <span className={`${styles.triangle} ${styles.blue}`}></span>
+        <span className={`${styles.triangle} ${styles.cyan1}`}></span>
+        <span className={`${styles.triangle} ${styles.green}`}></span>
+        <span className={`${styles.triangle} ${styles.cyan2}`}></span>
+      </div>
+      {children}
+    </>
+  );
+}
--- src/app/webapp/user/signout/page.tsx
+++ src/app/webapp/user/signout/page.tsx
@@ -1,42 +1,22 @@
 "use client";
-import { BorderRoundButton } from "@/components/buttons/BorderRoundButton";
-import { Thisapp } from "@/components/dummy/ThisApp";
-import { BottomNavigationBar } from "@/components/navigations/BottomNavigationBar";
 
 import styles from "./page.module.scss";
 
-export default function Test() {
+export default function SignOut() {
   const onClick = () => {
     console.log("clicked");
   };
 
   return (
-    <div className="relative h-full overflow-hidden">
-      <div className={styles.bg}>
-        <span className={`${styles.triangle} ${styles.blue}`}></span>
-        <span className={`${styles.triangle} ${styles.cyan1}`}></span>
-        <span className={`${styles.triangle} ${styles.green}`}></span>
-        <span className={`${styles.triangle} ${styles.cyan2}`}></span>
-      </div>
+    <div className="h-screen w-full overflow-hidden">
       <div className={styles.main}>
         <div className={styles.title}>アカウント</div>
-        <div className={styles.go}>
-          <div className={styles.text}>IMAP会員へようこそ !</div>
-          <div className={styles.button}>
-            <BorderRoundButton title="&emsp;Sign IN&emsp;" fontSize="text-2xl" onClick={onClick} />
+        <button className={styles.container} onClick={onClick}>
+          <div className={styles.inner}>
+            <span>&emsp;Sign IN&emsp;</span>
           </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <Thisapp>
-            <p className=" break-words">
-              こんにちは!!!!! 僕の名前は、、、、
-              ピカソのフルネームはパブロ・ディエーゴ・ホセ・フランシスコ・デ・パウラ・ホアン・ネポムセーノ・マリーア・デ・ロス・レメディオス・クリスピーン・クリスピアーノ・デ・ラ・サンティシマ・トリニダード・ルイス・イ・ピカソ。
-              です。。。 よろしくお願いします！！明日からよろしくお願いします！！
-            </p>
-          </Thisapp>
-        </div>
+        </button>
       </div>
-      <BottomNavigationBar />
     </div>
   );
 }
