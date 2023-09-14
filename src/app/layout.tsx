f5eb0f105be4b1cc89339a6f1f269737581b3edc
wiyco <lumiwslab@gmail.com>
2023-09-14 22:02:03 +0900

    Add `meta`s to `RootLayout`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,52 @@
 import "./globals.scss";
 
 export const metadata = {
-  title: "IMAP",
-  description: "PROJECT PIPLUP",
+  title: {
+    default: "IMAP",
+    template: "%s - IMAP",
+  },
+  applicationName: "IMAP",
+  description: "The Lunch Recommendation App for IPUT Students",
+  openGraph: {
+    title: "IMAP",
+    description: "The Lunch Recommendation App for IPUT Students",
+    type: "website",
+    locale: "ja_JP",
+    url: "/",
+    siteName: "IMAP",
+  },
+  icons: {
+    icon: [
+      {
+        src: "/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: {
+      src: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
+  themeColor: "#696969",
+  manifest: "/favicon/site.webmanifest",
+  appleWebApp: {
+    title: "IMAP",
+    statusBarStyle: "black-translucent",
+    startupImage: "/favicon/android-chrome-512x512.png",
+  },
+  alternates: {
+    maskIcon: "/favicon/safari-pinned-tab.svg",
+  },
+  other: {
+    msapplicationConfig: "/favicon/browserconfig.xml",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
