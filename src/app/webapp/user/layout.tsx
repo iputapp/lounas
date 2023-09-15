cd9c87bc9879a184c09fba9c0dc1c96a32995fef
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-16 01:22:36 +0900

    フォルダ移動

--- src/app/webapp/user/layout.tsx
+++ src/app/webapp/user/layout.tsx
@@ -1,7 +1,21 @@
+import React from "react";
+
+import styles from "./layout.module.scss";
+
 export const metadata = {
   title: "User",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
+  return (
+    <div className="relative h-full overflow-hidden">
+      <div className={styles.bg}>
+        <span className={`${styles.triangle} ${styles.blue}`}></span>
+        <span className={`${styles.triangle} ${styles.cyan1}`}></span>
+        <span className={`${styles.triangle} ${styles.green}`}></span>
+        <span className={`${styles.triangle} ${styles.cyan2}`}></span>
+      </div>
+      {children}
+    </div>
+  );
 }
