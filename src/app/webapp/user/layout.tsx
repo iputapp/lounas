badb1a45a1085989fb61cd01e61864c14ae73635
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-15 18:20:21 +0900

    背景が正しく表示されるようにrelative h-full overflow-hiddenの設定

--- src/app/webapp/user/layout.tsx
+++ src/app/webapp/user/layout.tsx
@@ -5,7 +5,7 @@ import styles from "./layout.module.scss";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="relative h-full overflow-hidden">
       <div className={styles.bg}>
         <span className={`${styles.triangle} ${styles.blue}`}></span>
         <span className={`${styles.triangle} ${styles.cyan1}`}></span>
@@ -13,6 +13,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <span className={`${styles.triangle} ${styles.cyan2}`}></span>
       </div>
       {children}
-    </>
+    </div>
   );
 }
