b76f730d21304a81cf107a462a0e68e36a654b26
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-17 05:19:54 +0900

    dummyコンポーネントの幅をw-fullに変更,そしてmarginを削除

--- src/app/test/dummy/page.tsx
+++ src/app/test/dummy/page.tsx
@@ -23,17 +23,21 @@ export default function Test() {
         <div className={styles.go}>
           <div className={styles.text}>IMAP会員へようこそ !</div>
           <div className={styles.button}>
-            <BorderRoundButton title="&emsp;Sign IN&emsp;" fontSize="text-2xl" onClick={onClick} />
+            <BorderRoundButton fontSize="text-2xl" onClick={onClick}>
+              &emsp;Sign IN&emsp;
+            </BorderRoundButton>
           </div>
         </div>
         <div className="flex items-center justify-center">
-          <Thisapp>
-            <p className=" break-words">
-              こんにちは!!!!! 僕の名前は、、、、
-              ピカソのフルネームはパブロ・ディエーゴ・ホセ・フランシスコ・デ・パウラ・ホアン・ネポムセーノ・マリーア・デ・ロス・レメディオス・クリスピーン・クリスピアーノ・デ・ラ・サンティシマ・トリニダード・ルイス・イ・ピカソ。
-              です。。。 よろしくお願いします！！明日からよろしくお願いします！！
-            </p>
-          </Thisapp>
+          <div className="w-5/6">
+            <Thisapp>
+              <p className=" break-words">
+                こんにちは!!!!! 僕の名前は、、、、
+                ピカソのフルネームはパブロ・ディエーゴ・ホセ・フランシスコ・デ・パウラ・ホアン・ネポムセーノ・マリーア・デ・ロス・レメディオス・クリスピーン・クリスピアーノ・デ・ラ・サンティシマ・トリニダード・ルイス・イ・ピカソ。
+                です。。。 よろしくお願いします！！明日からよろしくお願いします！！
+              </p>
+            </Thisapp>
+          </div>
         </div>
       </div>
       <BottomNavigationBar />
