b2fdd80adc66aa1fea4761ad5fd9da5ef9f4c6b6
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-16 19:00:20 +0900

    styles.module.scssに変更

--- src/components/dummy/ThisApp/index.tsx
+++ src/components/dummy/ThisApp/index.tsx
@@ -1,23 +1,23 @@
 "use client";
-import "./style.scss";
-
 import LogoFill from "@icons/logo-fill.svg";
+
+import styles from "./styles.module.scss";
 
 export function Thisapp({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container">
-      <div className="header">
-        <div className="logo">
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
           <LogoFill />
         </div>
-        <div className="title">IMΛP</div>
+        <div className={styles.title}>IMΛP</div>
       </div>
-      <div className="main">
-        <div className="about">このアプリについて</div>
-        <div className="box">{children}</div>
+      <div className={styles.main}>
+        <div className={styles.about}>このアプリについて</div>
+        <div className={styles.box}>{children}</div>
       </div>
-      <div className="footer">
-        <div className="developer">PROJECT RIPLUP アプリ開発サークル</div>
+      <div className={styles.footer}>
+        <div className={styles.developer}>PROJECT RIPLUP アプリ開発サークル</div>
       </div>
     </div>
   );
