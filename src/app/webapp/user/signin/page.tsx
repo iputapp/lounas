5848c50de5942ed52202aebe6f4900f40546fcd8
HIMA-HIMAZIN <taiyaki850@gmail.com>
2023-09-17 05:08:10 +0900

    signinページとsignoutページのデザインを合わせた。
    dummyコンポーネントに関したはコンフリクト回避の為一旦削除

--- src/app/webapp/user/signin/page.tsx
+++ src/app/webapp/user/signin/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { BorderRoundButton } from "@/components/buttons/BorderRoundButton";
-import { Thisapp } from "@/components/dummy/ThisApp";
 
 import styles from "./page.module.scss";
 
@@ -12,22 +11,23 @@ export default function Test() {
   return (
     <div className={styles.main}>
       <div className={styles.title}>アカウント</div>
-      <div className={styles.go}>
-        <div className={styles.text}>IMAP会員へようこそ !</div>
-        <div className={styles.button}>
+      <div className={styles.text}>IMAP会員へようこそ !</div>
+      <div className={styles.button}>
+        <div className={styles.size}>
           <BorderRoundButton fontSize="text-2xl" onClick={onClick}>
             &emsp;Sign IN&emsp;
           </BorderRoundButton>
         </div>
       </div>
+
       <div className="flex items-center justify-center">
-        <Thisapp>
-          <p className=" break-words">
+        <div className="h-full w-5/6">
+          <p className="break-words">
             こんにちは!!!!! 僕の名前は、、、、
             ピカソのフルネームはパブロ・ディエーゴ・ホセ・フランシスコ・デ・パウラ・ホアン・ネポムセーノ・マリーア・デ・ロス・レメディオス・クリスピーン・クリスピアーノ・デ・ラ・サンティシマ・トリニダード・ルイス・イ・ピカソ。
             です。。。 よろしくお願いします！！明日からよろしくお願いします！！
           </p>
-        </Thisapp>
+        </div>
       </div>
     </div>
   );
