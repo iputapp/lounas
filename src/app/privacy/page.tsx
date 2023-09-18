58cc91b15d25af05cc9c8a11960f9f0bdc1a4b54
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-19 01:29:16 +0900

    Review commit

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -20,49 +20,84 @@ export default function Page() {
   };
 
   return (
-    <main className={styles.main}>
-      <div className={styles.privacy}>
+    <>
+      <div className={styles.container}>
         <h1 className={styles.title}>データ利用</h1>
-        <p className={styles.text}>
-          このアプリではユーザーがより便利にお使いいただける用に情報提供の協力をお願いしています。
-        </p>
+        <div className={styles.description}>
+          <p>このアプリではユーザーがより便利にお使いいただくために情報提供をお願いしています。</p>
+          <button className={styles.dialoginfo} onClick={() => setIsOpen(!isOpen)}>
+            <span>私たちが考えるプライバシーについて詳しく知る</span>
+          </button>
+        </div>
+        <div className={styles.animation}>
+          <PrivacyPlayer />
+        </div>
+        <div className={styles.choice}>
+          <div className={styles.query}>
+            <span>同意いただけない場合は</span>
+            <span>ランキング機能のみのご利用になります。</span>
+          </div>
+          <div className={styles.button}>
+            <RectButton color="red" onClick={refuse}>
+              同意しない
+            </RectButton>
+            <RectButton color="blue" onClick={permission}>
+              同意する
+            </RectButton>
+          </div>
+        </div>
       </div>
-      <button onClick={() => setIsOpen(!isOpen)}>
-        <span className={styles.dialoginfo}>私たちが考えるプライバシーについて詳しく知る</span>
-      </button>
-      <DialogInfo title="確認コードが届かない" isOpen={isOpen} setIsOpen={setIsOpen}>
-        <article className="grid h-full w-full gap-8 text-sm font-semibold">
-          <section>確認コードが届かない場合は以下の対処法をお試しください。</section>
+      <DialogInfo title="私たちが考えるプライバシー" isOpen={isOpen} setIsOpen={setIsOpen}>
+        <article className="grid h-full w-full gap-8 text-xs font-semibold">
           <section>
-            <ol className="mx-auto grid w-fit list-decimal gap-4">
-              <li>迷惑メールを確認する</li>
-              <li>メールのフィルタ設定を確認する</li>
-              <li>ネットワーク状況を確認する</li>
+            <p>
+              私たちのアプリは、より良いユーザーエクスペリエンスを提供するために一部のデータを収集・トラッキングしています。
+            </p>
+            <p>このデータはパーソナライズやアプリの最適化に使用されます。</p>
+            <p>以下の項目について、ご了承いただきトラッキングの許可をお願いいたします。</p>
+          </section>
+          <section>
+            <ol className="mx-4 grid w-fit list-decimal gap-4">
+              <li>
+                <p>デバイス情報の収集</p>
+                <p>
+                  アプリの正常な動作とセキュリティ保護のため、デバイスの識別子やオペレーティングシステムのバージョンなどの情報を収集します。
+                </p>
+              </li>
+              <li>
+                <p>ユーザーアクティビティの追跡</p>
+                <p>
+                  アプリ内での操作や使用履歴の収集により、より適切な広告や推奨コンテンツを表示できるようになります。
+                </p>
+              </li>
+              <li>
+                <p>位置情報の取得</p>
+                <p>
+                  位置情報を利用して、近くの店舗やサービスの情報を提供する場合があります。位置情報は厳密に匿名化され、個別のユーザーを特定するためには使用されません。
+                </p>
+              </li>
+              <li>
+                <p>サードパーティとのデータ共有</p>
+                <p>
+                  私たちは、信頼できるパートナーと共有することで広告のターゲティングやアプリの改善を目指しています。ただし、個人を特定する情報は共有されません。
+                </p>
+              </li>
             </ol>
           </section>
           <section>
-            対処法をお試し頂いても解決しない場合は、お手数ですがサポートまでお問い合わせください。
+            <p>プライバシーとデータセキュリティは、私たちにとって非常に重要です。</p>
+            <p>私たちは収集されたデータを厳格に管理し、第三者には適切な 安全保護措置を講じます。</p>
+          </section>
+          <section>
+            <p>
+              このトラッキング許可は任意であり、アプリ内の設定からトラッキングのオプトアウトやオプトインを選択できます。
+            </p>
+          </section>
+          <section>
+            <p>ご不明な点や懸念事項がありましたら、いつでもお問い合わせください。</p>
           </section>
         </article>
       </DialogInfo>
-
-      <div className={styles.animation}>
-        <PrivacyPlayer />
-      </div>
-      <div className={styles.choice}>
-        <div className={styles.query}>
-          <span>同意いただけない場合は</span>
-          <span>ランキング機能のみのご利用になります。</span>
-        </div>
-        <div className={styles.button}>
-          <RectButton color="red" onClick={refuse}>
-            拒否する
-          </RectButton>
-          <RectButton color="blue" onClick={permission}>
-            許可する
-          </RectButton>
-        </div>
-      </div>
-    </main>
+    </>
   );
 }
