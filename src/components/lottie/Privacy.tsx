3097ac9ae6e6d12fe1a05ee3eb15538297a50caa
yoshi_tai <taiyaki850@gmail.com>
2023-09-19 02:02:13 +0900

    Feature/Privacy Page (#79)
    * jsonファイルの追加
    
    * test用scssの追加
    
    * test用page.tsxの追加
    
    * アニメーションのオートプレイとループ設定
    
    * アニメーションの位置を調整
    
    * test.pageの調整
    
    * 関数名の変更
    
    * div要素追加
    
    * scssの追加
    
    * test用pageにてdialogコンポーネントの追加
    
    * Dialoginfoの追加
    
    * dialogコンポーネントのファイル削除
    
    * scssのtextカラーの変更
    
    * プライバシー画面の大まかな位置を設定しました。
    
    * Rectbuttonコンポーネントの追加
    
    * アスペクト比1:1の追加
    
    * クラス名の変更
    
    * テスト用pageにdialoginfoコンポーネントを追加しました。
    
    * .dialoginfoの文字サイズを変更しました。
    
    * フォルダ整理
    
    * page.module.scssへの変更
    
    * テキストサイズの変更
    
    * フォルダ移動とh-screen,w-screen -> h-full,w-fullに変更
    
    * フォルダ移動
    
    * useRouterの使用
    
    拒否するとsignup.pageに移動
    同意するとhome.pageに移動
    
    * pl-5の削除
    
    * & > の削除
    
    * replaceへの変更
    
    * Modify dialog-info component
    
    - Rename prop
    - backgroud effects
    
    * Modify rect-button component
    
    - Cleanup
    - Change font-size
    
    * Delete unnecessary `div` tag
    
    * Add shadow to rect-button
    
    * Modify `safe-area-bottom`
    
    * Modify dialog-info component
    
    * Review commit
    
    * Replace `replace` to `push`
    
    * Update text color
    
    ---------
    
    Co-authored-by: wiyco <72733890+wiyco@users.noreply.github.com>

---
+++ src/components/lottie/Privacy.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export function PrivacyPlayer() {
+  return <Player src="/lottie/privacy-animation.json" autoplay loop />;
+}
