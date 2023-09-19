0afd8f8925a4f7e50a062fdb363d73581c986b83
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-20 03:44:33 +0900

    Modify transition func of bar

--- src/components/progresses/BasicLinearProgress/index.tsx
+++ src/components/progresses/BasicLinearProgress/index.tsx
@@ -3,7 +3,6 @@ import { styled } from "@mui/material/styles";
 
 const BorderLinearProgress = styled(LinearProgress)(() => ({
   height: 10,
-  overflow: "clip",
   borderRadius: 9999, // rounded-full
   [`&.${linearProgressClasses.colorPrimary}`]: {
     backgroundColor: "#e5e5e5",
@@ -12,6 +11,7 @@ const BorderLinearProgress = styled(LinearProgress)(() => ({
     borderRadius: 9999, // rounded-full
     background: "linear-gradient(to right, #0014ff, #07f)",
     mixBlendMode: "hard-light",
+    transition: "transform 0.4s cubic-bezier(0.65, 0, 0.35, 1)", // easeInOutCubic
   },
 }));
 
