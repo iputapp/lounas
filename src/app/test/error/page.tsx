8d6c22b2eac9c659e944f1465e6d01d3d513035b
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-19 17:25:51 +0900

    Rename file

--- src/app/test/error/page.tsx
+++ src/app/test/error/page.tsx
@@ -1,14 +1,27 @@
-import { Metadata } from "next";
+"use client";
 
+import { Metadata } from "next";
+import { useEffect } from "react";
+
+import { RectButton } from "@/components/buttons/RectButton";
 import { ErrorPlayer } from "@/components/lottie/Error";
 
-import styles from "./page.module.scss";
+import styles from "./error.module.scss";
 
 export const metadata: Metadata = {
   title: "Error",
 };
 
-export default function Error() {
+type ErrorProps = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+export default function Error({ error, reset }: ErrorProps) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>エラー</h1>
@@ -18,6 +31,11 @@ export default function Error() {
       <div className={styles.description}>
         <span className={styles.secondary}>認証コードの確認で問題が発生しました</span>
         <span className={styles.primary}>時間をおいて再度お試しください</span>
+        <div className={styles.button}>
+          <RectButton color="blue" onClick={reset}>
+            戻る
+          </RectButton>
+        </div>
       </div>
     </div>
   );
