cfa6763f64e0a02788e5ace99d3adf0a7d33d45f
yoshi_tai <taiyaki850@gmail.com>
2023-09-19 17:31:02 +0900

    Feature/Error Page (#80)
    * svg画像の追加
    
    * svg画像の色変更
    
    * test用page.tsxの作成
    
    * textサイズの変更と水平方向のmarginの削除
    
    * error.jsonの追加
    
    * error.jsonの追加
    
    * Error.jsonの再生設定
    
    * 画像をlottieに変更
    
    * animationのデザイン設定
    
    * フォルダ整理
    
    * フォルダ整理
    
    * フォルダ変更
    
    * Delete warning.svg
    
    * Move files
    
    * Cleanup root layout of test
    
    * Review commit
    
    * Add Metadata
    
    * Modify metadata of test layout
    
    * Rename file
    
    * Add copy to `/signup/otp`
    
    ---------
    
    Co-authored-by: wiyco <72733890+wiyco@users.noreply.github.com>

---
+++ src/components/lottie/Error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export function ErrorPlayer() {
+  return <Player src="/lottie/error-animation.json" autoplay loop />;
+}
