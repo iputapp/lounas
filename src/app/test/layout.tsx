43560247c64bd1a7a309d8f0e8bc9e8b40ddfeb7
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-19 13:46:43 +0900

    Cleanup root layout of test

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -1,13 +1,23 @@
-export const metadata = {
-  title: "Test | IMAP",
+import { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+
+import styles from "./layout.module.scss";
+
+export const metadata: Metadata = {
+  title: "Test",
   description: "A root page of test for Team PROJECT PIPLUP",
 };
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function TestLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <header></header>
-      <main>{children}</main>
+      <main className={`${styles.main} ${notoSansJP.className}`}>{children}</main>
       <footer></footer>
     </>
   );
