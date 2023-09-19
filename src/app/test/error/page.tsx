47ee3c490d9032cdff1d56fa1398d6be00cbe9c6
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-19 16:57:09 +0900

    Review commit

--- src/app/test/error/page.tsx
+++ src/app/test/error/page.tsx
@@ -2,15 +2,17 @@ import { ErrorPlayer } from "@/components/lottie/Error";
 
 import styles from "./page.module.scss";
 
-export default function Test() {
+export default function Error() {
   return (
-    <div className={styles.main}>
+    <div className={styles.container}>
       <h1 className={styles.title}>エラー</h1>
       <div className={styles.animation}>
         <ErrorPlayer />
       </div>
-      <p className={styles.text}>認証コード確認時に問題が発生しました</p>
-      <h2 className={styles.please}>時間をおいて再度お試しください</h2>
+      <div className={styles.description}>
+        <span className={styles.secondary}>認証コードの確認で問題が発生しました</span>
+        <span className={styles.primary}>時間をおいて再度お試しください</span>
+      </div>
     </div>
   );
 }
