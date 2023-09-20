d8494df10a41657d7a3a8e5ca02bc9e84817c317
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-21 01:27:40 +0900

    Revert "コンポーネントを修正しました。"
    This reverts commit 937260bc911e6d7e9488ed78feb22bd87c2162fc.

--- src/components/layouts/ExpandablePanel/index.tsx
+++ src/components/layouts/ExpandablePanel/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Cancel from "@icons/cancel.svg";
+import Enlarge from "@icons/enlarge.svg";
+import { useState } from "react";
 
 import styles from "./styles.module.scss";
 
@@ -9,35 +11,25 @@ type ExpandablePanelProps = {
   title: string;
   childrenEx?: React.ReactNode;
   titleEx?: string;
-  expanded: boolean;
-  setExpanded: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export function ExpandablePanel({
-  children,
-  title,
-  childrenEx,
-  titleEx,
-  expanded,
-  setExpanded,
-}: ExpandablePanelProps) {
+export function ExpandablePanel({ children, title, childrenEx, titleEx }: ExpandablePanelProps) {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <button
-      className={expanded ? `${styles.backdrop}` : "hidden"}
-      onClick={() => setExpanded(!expanded)}
-    >
-      <div className={`${styles.container} ${styles.expanded}`}>
+    <button className={expanded ? `${styles.backdrop}` : ""} onClick={() => setExpanded(!expanded)}>
+      <div className={`${styles.container} ${expanded ? styles.expanded : ""}`}>
         <div className={styles.content}>
-          <div className={`${styles.overview} ${styles.expanded}`}>
-            <h2 className={`${styles.title} ${styles.expanded}`}>{title}</h2>
+          <div className={`${styles.overview} ${expanded ? styles.expanded : ""}`}>
+            <h2 className={`${styles.title} ${expanded ? styles.expanded : ""}`}>{title}</h2>
             {children}
           </div>
-          <div className={`${styles.details} ${styles.expanded}`}>
+          <div className={`${styles.details} ${expanded ? styles.expanded : ""}`}>
             <h2 className={styles.title}>{titleEx}</h2>
             {childrenEx}
           </div>
         </div>
-        <Cancel />
+        <span>{expanded ? <Cancel /> : <Enlarge />}</span>
       </div>
     </button>
   );
