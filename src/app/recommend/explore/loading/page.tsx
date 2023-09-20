b827fd62325f818ab3260d363a37efc07932725f
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-20 17:23:41 +0900

    Review commit

--- src/app/recommend/explore/loading/page.tsx
+++ src/app/recommend/explore/loading/page.tsx
@@ -1,34 +1,28 @@
 "use client";
 
 import Skeleton from "@mui/material/Skeleton";
-import Stack from "@mui/material/Stack";
 import { useRouter } from "next/navigation";
-import * as React from "react";
 
 import { RectButton } from "@/components/buttons/RectButton";
 import { BorderTitle } from "@/components/headers/BorderTitle";
 
 import styles from "./page.module.scss";
 
-function Media() {
+function BasicSkeleton() {
   return (
-    <div className="grid gap-5">
-      <Stack spacing={1}>
-        <Skeleton variant="rounded" width={200} height={100} sx={{ bgcolor: "grey.700" }} />
-        <Skeleton variant="rounded" width={200} height={10} sx={{ bgcolor: "grey.700" }} />
-        <Skeleton variant="rounded" width={100} height={10} sx={{ bgcolor: "grey.700" }} />
-      </Stack>
-      <Stack spacing={1}>
-        <Skeleton variant="rounded" width={200} height={100} sx={{ bgcolor: "grey.700" }} />
-        <Skeleton variant="rounded" width={200} height={10} sx={{ bgcolor: "grey.700" }} />
-        <Skeleton variant="rounded" width={100} height={10} sx={{ bgcolor: "grey.700" }} />
-      </Stack>
+    <div className="flex h-14 w-full items-center justify-center space-x-4">
+      <Skeleton variant="rounded" width={"3.25rem"} height={"3.25rem"} className="bg-neutral-400" />
+      <div className="grid h-full content-around">
+        <Skeleton variant="rounded" width={"6rem"} height={"1rem"} className="bg-neutral-400" />
+        <Skeleton variant="rounded" width={"8rem"} height={"1rem"} className="bg-neutral-400" />
+      </div>
     </div>
   );
 }
 
-export default function LoadingPage() {
+export default function Page() {
   const router = useRouter();
+
   const cancel = () => {
     router.back();
   };
@@ -38,8 +32,9 @@ export default function LoadingPage() {
       <h1 className={styles.title}>
         <BorderTitle title="あなたの候補をもとに検索しています" fontSize="text-3xl" />
       </h1>
-      <div className={styles.media}>
-        <Media />
+      <div className={styles.skeleton}>
+        <BasicSkeleton />
+        <BasicSkeleton />
       </div>
       <div className={styles.button}>
         <RectButton color="red" onClick={cancel}>
