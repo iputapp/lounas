bd269ae8877a7b47b260add1c2d7d0559a257200
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-20 18:13:19 +0900

    Add delay to skeleton

--- src/app/recommend/explore/loading/page.tsx
+++ src/app/recommend/explore/loading/page.tsx
@@ -8,13 +8,31 @@ import { BorderTitle } from "@/components/headers/BorderTitle";
 
 import styles from "./page.module.scss";
 
-function BasicSkeleton() {
+function BasicSkeleton({ delay = 0 }: { delay?: number }) {
   return (
     <div className="flex h-14 w-full items-center justify-center space-x-4">
-      <Skeleton variant="rounded" width={"3.25rem"} height={"3.25rem"} className="bg-neutral-400" />
+      <Skeleton
+        variant="rounded"
+        width={"3.25rem"}
+        height={"3.25rem"}
+        sx={{ animationDelay: `${delay}s` }}
+        className="bg-neutral-400/80"
+      />
       <div className="grid h-full content-around">
-        <Skeleton variant="rounded" width={"6rem"} height={"1rem"} className="bg-neutral-400" />
-        <Skeleton variant="rounded" width={"8rem"} height={"1rem"} className="bg-neutral-400" />
+        <Skeleton
+          variant="rounded"
+          width={"6rem"}
+          height={"1rem"}
+          sx={{ animationDelay: `${delay + 0.3}s` }}
+          className="bg-neutral-400/80"
+        />
+        <Skeleton
+          variant="rounded"
+          width={"8rem"}
+          height={"1rem"}
+          sx={{ animationDelay: `${delay + 0.6}s` }}
+          className="bg-neutral-400/80"
+        />
       </div>
     </div>
   );
@@ -34,7 +52,7 @@ export default function Page() {
       </h1>
       <div className={styles.skeleton}>
         <BasicSkeleton />
-        <BasicSkeleton />
+        <BasicSkeleton delay={1} />
       </div>
       <div className={styles.button}>
         <RectButton color="red" onClick={cancel}>
