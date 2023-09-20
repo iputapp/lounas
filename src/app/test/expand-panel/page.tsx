214c2ee0e568b0664c4224bf8abbbd2110077bde
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-21 01:27:36 +0900

    Revert "test.pageを修正しました。"
    This reverts commit 3c867f855a596a8454571d3cfa058d560f8604b6.

--- src/app/test/expand-panel/page.tsx
+++ src/app/test/expand-panel/page.tsx
@@ -1,16 +1,10 @@
-"use client";
-import Drag from "@icons/drag.svg";
-import { useState } from "react";
-
 import { CardHorizontal } from "@/components/cards/CardHorizontal";
 import { ExpandablePanel } from "@/components/layouts/ExpandablePanel";
 import { PaymentGrid } from "@/components/lists/PaymentGrid";
 
-import styles from "./page.module.scss";
-
 export default function Test() {
   const now = new Date();
-  const [expanded, setExpanded] = useState(false);
+
   const data = {
     timeOpen: now.toLocaleTimeString("ja-JP", { hour: "2-digit", minute: "2-digit" }),
     timeClose: now.toLocaleTimeString("ja-JP", { hour: "2-digit", minute: "2-digit" }),
@@ -39,43 +33,12 @@ export default function Test() {
   return (
     <div className="h-screen w-full bg-gradient-to-b from-indigo-300 to-sky-300 p-4">
       <div className="grid gap-5">
-        <CardHorizontal
-          title="カード"
-          image="/icons/media-image.svg"
-          tag={1}
-          description=""
-          url=""
-        />
-        <CardHorizontal
-          title="カード"
-          image="/icons/media-image.svg"
-          tag={1}
-          description=""
-          url=""
-        />
-        <button className={styles.panel} onClick={() => setExpanded(!expanded)}>
-          <ul className={styles.panelContent}>
-            <li>
-              <p>
-                全日：{data.timeOpen}～{data.timeClose}
-              </p>
-            </li>
-            <li>
-              <p>滞在時間：おおよそ{Math.floor(data.timeStay / 60)}分</p>
-              <small className="text-xs">※混雑状況により異なります。</small>
-            </li>
-            <li>
-              <p>片道：おおよそ{Math.floor(data.timeDuration / 60)}分</p>
-            </li>
-          </ul>
-          <Drag />
-        </button>
+        <CardHorizontal title="カード" image="/icons/media-image.svg" tag={1} />
+        <CardHorizontal title="カード" image="/icons/media-image.svg" tag={1} />
         <ExpandablePanel
           title="店舗詳細"
           titleEx="決済方法"
           childrenEx={<PaymentGrid payments={data.payments} />}
-          setExpanded={setExpanded}
-          expanded={expanded}
         >
           <ul className="grid justify-items-start gap-4">
             <li>
