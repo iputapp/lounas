3396f1dfe23d72ba073e3d2bf1fd667644019156
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-20 14:16:36 +0900

    Modify props

--- src/app/recommend/result/page.tsx
+++ src/app/recommend/result/page.tsx
@@ -4,60 +4,73 @@ import { useRouter } from "next/navigation";
 
 import { RectButton } from "@/components/buttons/RectButton";
 import { CardHorizontal } from "@/components/cards/CardHorizontal";
-import { Payment } from "@/components/lists/Payment";
+import { PaymentShort, PaymentType } from "@/components/lists/PaymentShort";
 
 import styles from "./page.module.scss";
 
 export default function RecommendedResult() {
-  const payments = [
-    { payment: "クレジットカード", accepted: true },
-    { payment: "電子マネー", accepted: true },
-    { payment: "QRコード", accepted: false },
-  ];
   const router = useRouter();
+
+  /** テスト用 支払方法 */
+  const payments = [
+    { payment: "cash" as PaymentType, accepted: true },
+    { payment: "credit" as PaymentType, accepted: true },
+    { payment: "transport" as PaymentType, accepted: true },
+    { payment: "qr" as PaymentType, accepted: false },
+  ];
+
   const cancel = () => {
     router.replace("/webapp/home");
   };
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>あなたへのおすすめ</h1>
-      <div className={styles.main}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>あなたへのおすすめ</h1>
+      </div>
+      <div className={styles.content}>
         <CardHorizontal
+          url="/"
           title="かつ丼"
           tag={1}
           image="/test/ramen.webp"
-          description={<Payment payments={payments} />}
+          description={<PaymentShort payments={payments} />}
         />
         <CardHorizontal
+          url="/"
           title="かつ丼"
           tag={2}
           image="/test/ramen.webp"
-          description={<Payment payments={payments} />}
+          description={<PaymentShort payments={payments} />}
         />
         <CardHorizontal
+          url="/"
           title="かつ丼"
           tag={3}
           image="/test/ramen.webp"
-          description={<Payment payments={payments} />}
+          description={<PaymentShort payments={payments} />}
         />
         <CardHorizontal
+          url="/"
           title="かつ丼"
           tag={4}
           image="/test/ramen.webp"
-          description={<Payment payments={payments} />}
+          description={<PaymentShort payments={payments} />}
         />
         <CardHorizontal
+          url="/"
           title="かつ丼"
           tag={5}
           image="/test/ramen.webp"
-          description={<Payment payments={payments} />}
+          description={<PaymentShort payments={payments} />}
         />
       </div>
       <div className={styles.footer}>
-        <RectButton color="red" onClick={cancel}>
-          キャンセル
-        </RectButton>
+        <div className={styles.button}>
+          <RectButton color="red" onClick={cancel}>
+            キャンセル
+          </RectButton>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/lists/PaymentShort/index.tsx
@@ -0,0 +1,48 @@
+import Cancel from "@icons/cancel.svg";
+import Check from "@icons/check.svg";
+
+/** payment method  */
+export type PaymentType = "cash" | "credit" | "qr" | "transport";
+
+type PaymentShortProps = {
+  payment: PaymentType;
+  accepted: boolean;
+};
+
+export function PaymentShort({ payments }: { payments: PaymentShortProps[] }) {
+  const getPaymentName = (payment: PaymentType) => {
+    switch (payment) {
+      case "cash":
+        return "現金";
+      case "credit":
+        return "カード";
+      case "qr":
+        return "QRコード";
+      case "transport":
+        return "交通系";
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-end">
+      <ul className="w-fit">
+        {payments.map((item) => (
+          <li key={item.payment}>
+            <div className="flex items-center justify-end space-x-1">
+              <span>{getPaymentName(item.payment)}</span>
+              {item.accepted ? (
+                <span className="text-green-600">
+                  <Check />
+                </span>
+              ) : (
+                <span className="text-red-600">
+                  <Cancel />
+                </span>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
