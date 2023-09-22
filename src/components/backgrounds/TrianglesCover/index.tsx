b21b1d6b9ca9b7385711ce7d52c93bdd6a031dd3
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-22 14:37:21 +0900

    Fix undefined class-name

--- src/components/backgrounds/TrianglesCover/index.tsx
+++ src/components/backgrounds/TrianglesCover/index.tsx
@@ -3,9 +3,10 @@ import styles from "./styles.module.scss";
 export function TrianglesCover() {
   return (
     <div className={styles.bg}>
-      <span className={`${styles.circle} ${styles.blue}`}></span>
-      <span className={`${styles.circle} ${styles.cyan}`}></span>
-      <span className={`${styles.circle} ${styles.green}`}></span>
+      <span className={`${styles.triangle} ${styles.blue}`}></span>
+      <span className={`${styles.triangle} ${styles.cyan}`}></span>
+      <span className={`${styles.triangle} ${styles.green}`}></span>
+      <span className={`${styles.triangle} ${styles.lime}`}></span>
     </div>
   );
 }
