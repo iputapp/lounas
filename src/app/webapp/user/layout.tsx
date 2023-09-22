ae6990aba5d5ace006ac53cc4aa3a152f1f63654
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-22 14:18:34 +0900

    Cleanup codes

--- src/app/webapp/user/layout.tsx
+++ src/app/webapp/user/layout.tsx
@@ -11,9 +11,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <div className={styles.bg}>
         <span className={`${styles.triangle} ${styles.blue}`}></span>
-        <span className={`${styles.triangle} ${styles.cyan1}`}></span>
+        <span className={`${styles.triangle} ${styles.cyan}`}></span>
         <span className={`${styles.triangle} ${styles.green}`}></span>
-        <span className={`${styles.triangle} ${styles.cyan2}`}></span>
+        <span className={`${styles.triangle} ${styles.lime}`}></span>
       </div>
       {children}
     </>
--- src/app/webapp/user/signin/page.tsx
+++ src/app/webapp/user/signin/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 
 import { BorderRoundButton } from "@/components/buttons/BorderRoundButton";
