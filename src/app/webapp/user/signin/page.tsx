d0b38b9d6e7b832ac5f37c626c246da0e09d28c5
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-22 17:24:53 +0900

    Review commit of sign-in page

--- src/app/webapp/user/signin/page.tsx
+++ src/app/webapp/user/signin/page.tsx
@@ -10,28 +10,38 @@ import styles from "./page.module.scss";
 export default function Page() {
   const router = useRouter();
   const signOut = () => {
-    router.push("/webapp/user/signout");
+    router.replace("/webapp/user/signout");
   };
 
   return (
-    <div className={styles.main}>
+    <div className={styles.container}>
       <h1 className={styles.title}>アカウント</h1>
-
-      <div className={styles.text}>IMAP会員へようこそ !</div>
-
-      <div className={styles.button}>
-        <BorderRoundButton fontSize="text-2xl" onClick={signOut}>
-          &emsp;Sign IN&emsp;
-        </BorderRoundButton>
+      <div className={styles.sub}>
+        <span className={styles.text}>IMAP会員へようこそ !</span>
+        <div className={styles.button}>
+          <BorderRoundButton fontSize="text-2xl" onClick={signOut}>
+            Sign In
+          </BorderRoundButton>
+        </div>
       </div>
-
-      <DummyPanel>
-        <p className="break-words">
-          こんにちは!!!!! 僕の名前は、、、、
-          ピカソのフルネームはパブロ・ディエーゴ・ホセ・フランシスコ・デ・パウラ・ホアン・ネポムセーノ・マリーア・デ・ロス・レメディオス・クリスピーン・クリスピアーノ・デ・ラ・サンティシマ・トリニダード・ルイス・イ・ピカソ。
-          です。。。 よろしくお願いします！！明日からよろしくお願いします！！
-        </p>
-      </DummyPanel>
+      <div className={styles.panel}>
+        <DummyPanel>
+          <div className="grid gap-4 text-neutral-800">
+            <section className="grid justify-items-center">
+              <p>このアプリについて</p>
+            </section>
+            <section>
+              <p>私たちは、多様なバックグラウンドや経験を持つ個々の個人から成るグループです。</p>
+              <p>
+                共に協力し合い、様々な問題を解決していく中で、私たちの成長と共に、より良い未来を築くことを目標としています。
+              </p>
+              <p>
+                互いに尊重し、協力し、理解し合うことを大切にし、さまざまな視点やアイデアを活用していきます。
+              </p>
+            </section>
+          </div>
+        </DummyPanel>
+      </div>
     </div>
   );
 }
