89a6c067e0233b178c15c80fca9ea22134ae1dad
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-23 02:01:17 +0900

    Add link to setting list

--- src/app/webapp/user/signout/page.tsx
+++ src/app/webapp/user/signout/page.tsx
@@ -3,6 +3,7 @@
 import Lock from "@icons/lock.svg";
 import NavArrowRight from "@icons/nav-arrow-right.svg";
 import Send from "@icons/send.svg";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import styles from "./page.module.scss";
@@ -14,10 +15,14 @@ export default function Page() {
     {
       title: "プライバシー",
       icon: <Lock />,
+      url: "/privacy",
+      isInternal: true,
     },
     {
       title: "お問い合わせ",
       icon: <Send />,
+      url: "mailto:info&#64;example.com",
+      isInternal: false,
     },
   ];
 
@@ -36,17 +41,29 @@ export default function Page() {
       </div>
       <div className={styles.panel}>
         <article>
-          {listItems.map((item, index) => (
-            <section key={index}>
-              <div>
-                <span>{item.icon}</span>
-                <span>{item.title}</span>
-              </div>
-              <span>
-                <NavArrowRight />
-              </span>
-            </section>
-          ))}
+          {listItems.map((item, index) =>
+            item.isInternal ? (
+              <Link key={index} href={item.url}>
+                <div>
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </div>
+                <span>
+                  <NavArrowRight />
+                </span>
+              </Link>
+            ) : (
+              <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">
+                <div>
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </div>
+                <span>
+                  <NavArrowRight />
+                </span>
+              </a>
+            )
+          )}
         </article>
       </div>
     </div>
