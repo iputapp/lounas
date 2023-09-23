f023ab59dfc7149f80d7b92c13fcd4d9ba0a5f04
yoshi_tai <taiyaki850@gmail.com>
2023-09-24 04:03:44 +0900

    Feature/Selection Stack (#99)
    * test.page作成
    
    usestateを使用して絞り込み画面で選択したボタンによって色とtextを取得して表示するbarを作成した。
    
    * selection-stackコンポーネントの作成
    
    textを反映されました。
    色を反映させる機能は作成中です。
    
    * selection-stackコンポーネントの見た目を作成しました。
    
    * SelectionStackButton関数の削除
    
    * Delete previous files
    
    * Delete test page
    
    * Add selection-stack component
    
    * Delete scss imported in template
    
    ---------
    
    Co-authored-by: wiyco <72733890+wiyco@users.noreply.github.com>

--- src/app/test/circle-button/template.tsx
+++ src/app/test/circle-button/template.tsx
@@ -4,9 +4,9 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import { CircleButton } from "@/components/buttons/CircleButton";
+import { SelectionStack } from "@/components/widgets/SelectionStack";
 
 import { selections, SelectionType } from "./constants";
-import styles from "./template.module.scss";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -33,57 +33,53 @@ export default function Template({ children }: { children: React.ReactNode }) {
     <>
       {children}
       {/* selection stack */}
-      <div
-        className={`fixed inset-x-0 bottom-12 z-30 mx-auto aspect-[3.64/1] w-[80%] max-w-sm overflow-clip rounded-full bg-neutral-100/80 backdrop-blur ${styles.neumo}`}
-      >
-        <div className="grid h-full w-full grid-cols-3 place-items-center py-2 text-xl">
-          {/* quantity */}
-          {selected?.quantity && (
-            <CircleButton
-              title={selected.quantity.title}
-              value={selected.quantity.value}
-              position="relative"
-              gradient={selected.quantity.gradient}
-              animation={{ delay: 0, stiffness: 200, damping: 20 }}
-              onClick={() =>
-                router.push(
-                  `quantity?${new URLSearchParams(Array.from(searchParams.entries())).toString()}`
-                )
-              }
-            />
-          )}
-          {/* price */}
-          {selected?.price && (
-            <CircleButton
-              title={selected.price.title}
-              value={selected.price.value}
-              position="relative"
-              gradient={selected.price.gradient}
-              animation={{ delay: 0.1, stiffness: 200, damping: 20 }}
-              onClick={() =>
-                router.push(
-                  `price?${new URLSearchParams(Array.from(searchParams.entries())).toString()}`
-                )
-              }
-            />
-          )}
-          {/* taste */}
-          {selected?.taste && (
-            <CircleButton
-              title={selected.taste.title}
-              value={selected.taste.value}
-              position="relative"
-              gradient={selected.taste.gradient}
-              animation={{ delay: 0.2, stiffness: 200, damping: 20 }}
-              onClick={() =>
-                router.push(
-                  `taste?${new URLSearchParams(Array.from(searchParams.entries())).toString()}`
-                )
-              }
-            />
-          )}
-        </div>
-      </div>
+      <SelectionStack>
+        {/* quantity */}
+        {selected?.quantity && (
+          <CircleButton
+            title={selected.quantity.title}
+            value={selected.quantity.value}
+            position="relative"
+            gradient={selected.quantity.gradient}
+            animation={{ delay: 0, stiffness: 200, damping: 20 }}
+            onClick={() =>
+              router.push(
+                `quantity?${new URLSearchParams(Array.from(searchParams.entries())).toString()}`
+              )
+            }
+          />
+        )}
+        {/* price */}
+        {selected?.price && (
+          <CircleButton
+            title={selected.price.title}
+            value={selected.price.value}
+            position="relative"
+            gradient={selected.price.gradient}
+            animation={{ delay: 0.1, stiffness: 200, damping: 20 }}
+            onClick={() =>
+              router.push(
+                `price?${new URLSearchParams(Array.from(searchParams.entries())).toString()}`
+              )
+            }
+          />
+        )}
+        {/* taste */}
+        {selected?.taste && (
+          <CircleButton
+            title={selected.taste.title}
+            value={selected.taste.value}
+            position="relative"
+            gradient={selected.taste.gradient}
+            animation={{ delay: 0.2, stiffness: 200, damping: 20 }}
+            onClick={() =>
+              router.push(
+                `taste?${new URLSearchParams(Array.from(searchParams.entries())).toString()}`
+              )
+            }
+          />
+        )}
+      </SelectionStack>
     </>
   );
 }
