af89c5a05e2a0f435881abe6b19cf47f7c568d26
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-24 01:16:39 +0900

    Update result page for test

--- src/app/test/circle-button/result/page.tsx
+++ src/app/test/circle-button/result/page.tsx
@@ -7,9 +7,9 @@ export default function Test() {
   const currentParams = Array.from(searchParams.entries());
 
   return (
-    <div className="grid h-screen w-full grid-rows-6 place-items-center">
-      <h1 className="row-start-3 text-xl">あなたの候補をもとに検索しています...</h1>
-      <ul className="row-start-4 text-base text-gray-500">
+    <div className="grid h-full w-full content-center gap-6">
+      <h1 className="mx-auto w-fit text-lg">あなたの候補をもとに検索しています...</h1>
+      <ul className="mx-auto text-base text-neutral-700">
         {currentParams.map((param, index) => (
           <li key={index}>
             <span>{param[0].charAt(0).toUpperCase() + param[0].slice(1).toLowerCase()}: </span>
