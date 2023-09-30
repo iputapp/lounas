627e95f1860f9d591f11fc0f02f867195d1afe0a
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-30 19:52:23 +0900

    Disable for testing otp-page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
 /**
  * @param {NextRequest} request
@@ -7,24 +7,25 @@ import { NextResponse } from "next/server";
  * @see {@link https://nextjs.org/docs/app/building-your-application/routing/middleware}
  */
 export function middleware(request: NextRequest) {
-  /** @example /signup/otp?pending */
-  if (
-    request.nextUrl.pathname.endsWith("/otp") &&
-    request.nextUrl.searchParams.has(process.env.NEXT_PUBLIC_QUERY_SIGN_UP ?? "pending")
-  ) {
-    const value = request.cookies.get(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up")
-      ?.value;
-    if (value === process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_VALUE ?? "sign-up-success") {
-      request.cookies.delete(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up");
-      return NextResponse.rewrite(new URL("/signup/otp", request.url));
-    } else {
-      request.cookies.delete(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up");
-      return NextResponse.redirect(new URL("/signup", request.url));
-    }
-  } else {
-    request.cookies.delete(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up");
-    return NextResponse.redirect(new URL("/signup", request.url));
-  }
+  console.log(request.nextUrl.pathname);
+  // /** @example /signup/otp?pending */
+  // if (
+  //   request.nextUrl.pathname.endsWith("/otp") &&
+  //   request.nextUrl.searchParams.has(process.env.NEXT_PUBLIC_QUERY_SIGN_UP ?? "pending")
+  // ) {
+  //   const value = request.cookies.get(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up")
+  //     ?.value;
+  //   if (value === process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_VALUE ?? "sign-up-success") {
+  //     request.cookies.delete(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up");
+  //     return NextResponse.rewrite(new URL("/signup/otp", request.url));
+  //   } else {
+  //     request.cookies.delete(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up");
+  //     return NextResponse.redirect(new URL("/signup", request.url));
+  //   }
+  // } else {
+  //   request.cookies.delete(process.env.NEXT_PUBLIC_COOKIE_SIGN_UP_NAME ?? "sign-up");
+  //   return NextResponse.redirect(new URL("/signup", request.url));
+  // }
 }
 
 export const config = {
