05507d7e6c6eeaeb546bfd38031902c696b4f0b4
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-30 19:59:08 +0900

    Update meta of sign-up page

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Sign up",
-  description: "This is a sign up page. You can create an account from this page.",
+  description: "This is the sign-up page. You can create an account from this page.",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
