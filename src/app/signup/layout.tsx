0aad024abf5e287510e90c7483fda53309e43906
wiyco <72733890+wiyco@users.noreply.github.com>
2023-09-30 21:22:02 +0900

    Add styles to main element of sign-up

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,15 +1,23 @@
 import { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+
+import styles from "./layout.module.scss";
 
 export const metadata: Metadata = {
   title: "Sign up",
   description: "This is the sign-up page. You can create an account from this page.",
 };
 
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <header></header>
-      {children}
+      <main className={`${styles.main} ${notoSansJP.className}`}>{children}</main>
       <footer></footer>
     </>
   );
