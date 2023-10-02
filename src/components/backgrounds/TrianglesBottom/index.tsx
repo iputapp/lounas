b7fe6146c1e134a738d84477ae2000119e5def16
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-02 23:57:58 +0900

    Fix typo

--- src/components/backgrounds/TrianglesBottom/index.tsx
+++ src/components/backgrounds/TrianglesBottom/index.tsx
@@ -5,12 +5,12 @@ type TrianglesBottomProps = {
   zIndex?: "-z-0" | "-z-10" | "-z-20" | "-z-30" | "-z-40" | "-z-50" | "z-auto";
 };
 
-export function TrianglesBottom({ move = false, zIndex = "-z-50" }: TrianglesBottomProps) {
+export function TrianglesBottom({ move = true, zIndex = "-z-50" }: TrianglesBottomProps) {
   return (
     <div className={`${styles.bg} ${zIndex}`}>
-      <span className={`${styles.triangle} ${styles.blue} ${move ? "move" : ""}`}></span>
-      <span className={`${styles.triangle} ${styles.cyan} ${move ? "move" : ""}`}></span>
-      <span className={`${styles.triangle} ${styles.green} ${move ? "move" : ""}`}></span>
+      <span className={`${styles.triangle} ${styles.blue} ${move ? styles.move : ""}`}></span>
+      <span className={`${styles.triangle} ${styles.cyan} ${move ? styles.move : ""}`}></span>
+      <span className={`${styles.triangle} ${styles.green} ${move ? styles.move : ""}`}></span>
     </div>
   );
 }
