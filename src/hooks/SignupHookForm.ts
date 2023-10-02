a08e7030065d82142181606fb16c553947815851
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-02 09:21:46 +0900

    Add sign-up hook-form

---
+++ src/hooks/SignupHookForm.ts
@@ -0,0 +1,53 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, useWatch } from "react-hook-form";
+import { z } from "zod";
+
+// import { UserSchema } from "@/lib/zod";
+
+/** form schema */
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "メールアドレスを入力してください。" })
+    .email({ message: "不正なメールアドレスです。" }),
+  agreePolicy: z.literal(true, { errorMap: () => ({ message: "同意が必要です。" }) }),
+});
+/** type of form schema */
+type FormSchema = z.infer<typeof formSchema>;
+
+/** sign-up hook form */
+const SignupHookForm = () => {
+  /** hook form */
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    defaultValues: undefined,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    resolver: zodResolver(formSchema),
+  });
+
+  const watchedInput = useWatch({ control });
+
+  console.log("errors", errors);
+  console.log("watchedInput", watchedInput);
+
+  /** called only when the value conversion and type checking for zod pass */
+  const onSubmit = (data: FormSchema): void => {
+    console.log("data", data);
+  };
+
+  return {
+    form: {
+      control,
+      handleSubmit,
+      onSubmit,
+      errors,
+    },
+  };
+};
+
+export { SignupHookForm };
