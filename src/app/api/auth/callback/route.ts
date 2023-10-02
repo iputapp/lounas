38ac3d77d71c90b1128d2037ca79afbd4302278a
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-03 02:49:39 +0900

    Cleanup codes

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,13 +3,13 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const requestURL = new URL(request.url);
-  const code = requestURL.searchParams.get("code");
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(requestURL.origin);
+  return NextResponse.redirect(requestUrl.origin);
 }
