e79f63c4dacc68152c697d7f17ec3fac795508c5
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-03 22:22:26 +0900

    Add validation to otp input  field

--- src/app/api/auth/otp/verification/index.ts
+++ src/app/api/auth/otp/verification/index.ts
@@ -1,20 +1,12 @@
 import { z } from "zod";
 
-import { signupSchema } from "..";
-
 /** verification schema */
-const verificationSchema = signupSchema
-  .pick({
-    email: true,
-  })
-  .merge(
-    z.object({
-      password: z
-        .string()
-        .length(6, { message: "6桁の数字を入力してください。" })
-        .regex(/^[0-9]+$/, { message: "数字のみを入力してください。" }),
-    })
-  );
+const verificationSchema = z.object({
+  password: z
+    .string()
+    .length(6, { message: "6桁の数字を入力してください。" })
+    .regex(/^[0-9]+$/, { message: "数字のみを入力してください。" }),
+});
 
 /** type of verification schema */
 type Verification = z.infer<typeof verificationSchema>;
