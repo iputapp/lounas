af5c7b4ee4ae0aa3783af67ade69cf0e54db6508
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-03 10:45:00 +0900

    Replace validation schema to import

--- src/app/api/auth/otp/verification/route.ts
+++ src/app/api/auth/otp/verification/route.ts
@@ -1,62 +1,59 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { z } from "zod";
 
-import { EMAIL_DOMAIN } from "@/constants";
-import { sign } from "jsonwebtoken";
-import { serialize } from "cookie";
+// import { sign } from "jsonwebtoken";
+// import { serialize } from "cookie";
+import type { Verification } from ".";
+import { verificationSchema } from ".";
 
-const MAX_AGE = 60 * 60 * 24 * 30; // 30 days
+/** @see{@link https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config} */
+export const dynamic = "force-dynamic";
 
-export const OtpVerifyRequestSchema = z.object({
-  email: z.string().email().endsWith(EMAIL_DOMAIN),
-  password: z.string().length(6),
-});
-
-export type OtpVerifyRequest = z.infer<typeof OtpVerifyRequestSchema>;
+const onlyPasswordSchema = verificationSchema.pick({ password: true });
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  const payload: OtpVerifyRequest = OtpVerifyRequestSchema.parse(body);
+  const body = (await request.json()) as Promise<Verification>;
+  const payload = onlyPasswordSchema.safeParse(body);
+
+  console.log("payload-verify", payload);
+
+  if (!payload.success) return NextResponse.error();
+
+  const cookieStore = cookies();
+  const email = cookieStore.get("email");
+  console.log(email);
+
+  if (!email) return NextResponse.error();
 
   const supabase = createRouteHandlerClient({ cookies });
-
   const { data, error } = await supabase.auth.verifyOtp({
-    email: payload.email,
-    token: payload.password,
     type: "email",
+    email: String(email),
+    token: payload.data.password,
+    options: {
+      redirectTo: `${new URL(request.url).origin}/api/auth/callback`,
+    },
   });
 
-  if (error) {
-    return NextResponse.error();
-  }
+  if (error || !data) return NextResponse.error();
 
-  if (data) {
-    const secret = process.env.JWT_SECRET;
-    const token = sign(
-      {
-        payload.email,
-      }, 
-      secret, 
-      { 
-        expiresIn: MAX_AGE,
-      });
+  // const secret = process.env.JWT_SECRET;
+  // const token = sign(
+  //   {
+  //     payload.email,
+  //   },
+  //   secret,
+  //   {
+  //     expiresIn: MAX_AGE,
+  //   });
 
-    const serialized = serialize("OutSiteJWT", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: MAX_AGE,
-      path: "/",});
+  // const serialized = serialize("OutSiteJWT", token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV === "production",
+  //   sameSite: "strict",
+  //   maxAge: MAX_AGE,
+  //   path: "/",});
 
-    const response = {
-      message: "Authenticated!"
-    }
-
-    return new Response(JSON.stringify(response), {
-      status: 200,
-      headers: { "Set-Cookie": serialized },
-    });
-  }
+  return NextResponse.json({ message: "authenticated" });
 }
