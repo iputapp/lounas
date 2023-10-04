c2a725c83b6a9f3d8b6d12060900265d4db447c6
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-04 13:13:42 +0900

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,67 +5,74 @@ import { NextResponse } from "next/server";
 /** @see {@link https://nextjs.org/docs/app/building-your-application/routing/middleware} */
 export async function middleware(req: NextRequest) {
   const reqUrl = new URL(req.url);
+  const reqNextPath = req.nextUrl.pathname;
   /** supabase auth session */
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
   const session = await supabase.auth.getSession();
 
-  console.log("session", session);
+  // console.log(session);
 
-  /** /signup/verify */
-  if (req.nextUrl.pathname.endsWith("/verify")) {
-    /** pending status */
-    if (req.cookies.has("verify")) {
-      res.cookies.delete("verify");
-      return;
-    } else {
+  /** signined */
+  if (session.data.session) {
+    /** root */
+    if (reqNextPath === "/") {
+      return NextResponse.redirect(`${reqUrl.origin}/webapp`);
+    }
+
+    /** ----- auth ----- */
+    /** /webapp/user */
+    if (reqNextPath.endsWith("/signin")) {
+      return NextResponse.redirect(`${reqUrl.origin}/webapp/user/signout`);
+    }
+  } else {
+    /** root */
+    if (reqNextPath === "/") {
       return NextResponse.redirect(`${reqUrl.origin}/signup`);
     }
-  }
 
-  /** /privacy */
-  if (req.nextUrl.pathname.startsWith("/privacy")) {
-    if (session.data) {
-      return;
-    } else {
-      return NextResponse.redirect(`${reqUrl.origin}`);
-    }
-  }
-
-  /** /dish */
-  if (req.nextUrl.pathname.startsWith("/dish")) {
-    if (session.data) {
-      return;
-    } else {
-      return NextResponse.redirect(`${reqUrl.origin}`);
-    }
-  }
-
-  /** /restaurant */
-  if (req.nextUrl.pathname.startsWith("/restaurant")) {
-    if (session.data) {
-      return;
-    } else {
-      return NextResponse.redirect(`${reqUrl.origin}`);
-    }
-  }
-
-  /** /webapp/user */
-  if (req.nextUrl.pathname.endsWith("/signout")) {
-    if (session.data) {
-      return;
-    } else {
+    /** ----- auth ----- */
+    /** /webapp/user */
+    if (reqNextPath.endsWith("/signout")) {
       return NextResponse.redirect(`${reqUrl.origin}/webapp/user/signin`);
+    }
+
+    /** /signup/verify */
+    if (reqNextPath.endsWith("/verify")) {
+      /** pending verification */
+      if (!req.cookies.has("verify")) return NextResponse.redirect(`${reqUrl.origin}`);
+
+      /** remove access permissions to the verify page */
+      res.cookies.delete("verify");
+      return;
+    }
+
+    /** ----- settings ----- */
+    /** /privacy */
+    if (reqNextPath.startsWith("/privacy")) {
+      return NextResponse.redirect(`${reqUrl.origin}`);
+    }
+
+    /** ----- data ----- */
+    /** /dish */
+    if (reqNextPath.startsWith("/dish")) {
+      return NextResponse.redirect(`${reqUrl.origin}`);
+    }
+
+    /** /restaurant */
+    if (reqNextPath.startsWith("/restaurant")) {
+      return NextResponse.redirect(`${reqUrl.origin}`);
     }
   }
 }
 
 export const config = {
   matcher: [
+    "/",
     "/signup/:path+",
     "/privacy/:path*",
     "/dish/:path*",
     "/restaurant/:path*",
-    "/webapp/user/signout",
+    "/webapp/:path+",
   ],
 };
