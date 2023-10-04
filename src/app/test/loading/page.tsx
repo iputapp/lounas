3c221e372eb99bc5bec25a1ff286a042a10f3658
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-05 02:14:15 +0900

    Fix typo

--- src/app/test/loading/page.tsx
+++ src/app/test/loading/page.tsx
@@ -4,14 +4,14 @@ export default function Test() {
   return (
     <>
       <div className="grid grid-cols-2 gap-6 p-6">
-        <span>I&apos;M a MESS&rsquo;...</span>
-        <span>I&apos;M a MESS&rsquo;...</span>
-        <span>I&apos;M a MESS&rsquo;...</span>
+        <span>I&apos;M a MESS...</span>
+        <span>I&apos;M a MESS...</span>
+        <span>I&apos;M a MESS...</span>
       </div>
       <div className="mx-auto grid w-fit grid-cols-2 place-items-center gap-8 px-5">
-        <span>I&apos;M a MESS&rsquo;...</span>
-        <span>I&apos;M a MESS&rsquo;...</span>
-        <span>I&apos;M a MESS&rsquo;...</span>
+        <span>I&apos;M a MESS...</span>
+        <span>I&apos;M a MESS...</span>
+        <span>I&apos;M a MESS...</span>
       </div>
       <LoadingLayer />
     </>
