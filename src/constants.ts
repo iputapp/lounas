2f608bf32b2f102a82d9dafafade835d3a95abee
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-04 13:23:52 +0900

    Comment-out unused constant-vals
    - Delete this file eventually.

--- src/constants.ts
+++ src/constants.ts
@@ -1,34 +1,34 @@
-/**
- * @constant {number} STUDENT_ID_LENGTH
- *
- * Length of `student ID`
- */
-export const STUDENT_ID_LENGTH = 6;
+// /**
+//  * @constant {number} STUDENT_ID_LENGTH
+//  *
+//  * Length of `student ID`
+//  */
+// export const STUDENT_ID_LENGTH = 6;
 
-/**
- * @constant {RegExp} STUDENT_ID_REGEX
- *
- * Regex of `student ID`
- */
-export const STUDENT_ID_REGEX = /^(tk|TK)?2[0-9]{5}$/;
+// /**
+//  * @constant {RegExp} STUDENT_ID_REGEX
+//  *
+//  * Regex of `student ID`
+//  */
+// export const STUDENT_ID_REGEX = /^(tk|TK)?2[0-9]{5}$/;
 
-/**
- * @constant {string} EMAIL_DOMAIN
- *
- * Domain of `email`
- */
-export const EMAIL_DOMAIN = "tks.iput.ac.jp";
+// /**
+//  * @constant {string} EMAIL_DOMAIN
+//  *
+//  * Domain of `email`
+//  */
+// export const EMAIL_DOMAIN = "tks.iput.ac.jp";
 
-/**
- * @constant {number} VERIFY_DIGITS_LENGTH
- *
- * Length of `verify digits`
- */
-export const VERIFY_DIGITS_LENGTH = 6;
+// /**
+//  * @constant {number} VERIFY_DIGITS_LENGTH
+//  *
+//  * Length of `verify digits`
+//  */
+// export const VERIFY_DIGITS_LENGTH = 6;
 
-/**
- * @constant {RegExp} VERIFY_DIGITS_REGEX
- *
- * Regex of `verify digits`
- */
-export const VERIFY_DIGITS_REGEX = /^[0-9]{6}$/;
+// /**
+//  * @constant {RegExp} VERIFY_DIGITS_REGEX
+//  *
+//  * Regex of `verify digits`
+//  */
+// export const VERIFY_DIGITS_REGEX = /^[0-9]{6}$/;
