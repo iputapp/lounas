d0c9798190b70b6f37aff09f53916c6de1567629
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-04 16:58:34 +0900

    Fix signup page styles

--- src/app/signup/(otp)/page.tsx
+++ src/app/signup/(otp)/page.tsx
@@ -16,9 +16,6 @@ export default function Page() {
       <div className={styles.container}>
         <h1 className={styles.title}>ようこそ</h1>
         <SignupForm className={styles.form} />
-        <div className={styles.additional}>
-          <Link href={"/webapp"}>登録せずに試す&#40;ランキング機能のみ&#41;</Link>
-        </div>
         <div className={styles.tos}>
           <Link href={"/tos"} target="_blank" rel="noopener noreferrer">
             <span>
@@ -29,6 +26,9 @@ export default function Page() {
               <NavArrowLeft />
             </span>
           </Link>
+        </div>
+        <div className={styles.additional}>
+          <Link href={"/webapp"}>登録せずに試す&#40;ランキング機能のみ&#41;</Link>
         </div>
       </div>
     </>
