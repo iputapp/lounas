d54ccab73fc98e8bad7a6d966eb22cb2640cb19c
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-05 02:23:18 +0900

    Add global-error

---
+++ src/app/error.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Metadata } from "next";
+import { useEffect } from "react";
+
+import { RectButton } from "@/components/buttons/RectButton";
+import { ErrorPlayer } from "@/components/lottie/Error";
+
+import styles from "./error.module.scss";
+
+export const metadata: Metadata = {
+  title: "Error",
+};
+
+type GlobalErrorProps = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+export default function GlobalError({ error, reset }: GlobalErrorProps) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>エラー</h1>
+      <div className={styles.animation}>
+        <ErrorPlayer />
+      </div>
+      <div className={styles.description}>
+        <span className={styles.secondary}>問題が発生しました</span>
+        <span className={styles.primary}>時間をおいて再度お試しください</span>
+        <div className={styles.button}>
+          <RectButton color="blue" onClick={reset}>
+            戻る
+          </RectButton>
+        </div>
+      </div>
+    </div>
+  );
+}
