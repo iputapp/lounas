b3be1fde04a1a71e380f79b556315fa1d1ad202d
nafell <pixel68.43@gmail.com>
2023-10-07 13:16:19 +0900

    API Route: restaurant/navi prototype, untested

---
+++ src/app/api/v-beta/restaurant/[id]/navi/route.ts
@@ -0,0 +1,49 @@
+import prisma from "@/lib/prisma";
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  const restaurantId = params.id;
+
+  const isIdValid = verifyUrlId(restaurantId);
+  if (!isIdValid) return isIdValid;
+
+  const routeSteps = await prisma.route.findMany({
+    where: { restaurantId: restaurantId },
+    include: {
+      routeType: true,
+    },
+  });
+
+  const firstStep = routeSteps.find((step) => !step.previousStepId);
+  if (!firstStep) {
+    return new Response("Data error: Cannot find first step of route.", { status: 500 });
+  }
+
+  const sortedRoutes = [firstStep];
+  let currentStep = firstStep;
+  const count = 0;
+  const length = routeSteps.length;
+  while (currentStep.nextStepId && currentStep) {
+    if (count >= length) {
+      return new Response("Data error: Route step chain is too long.", { status: 500 });
+      // nextStepIdがループしている
+    }
+
+    const nextStep = routeSteps.find((step) => step.id == currentStep.nextStepId);
+    if (!nextStep) {
+      return new Response(
+        "Data error: While step chain(nextStepId) is defined, the actual record was not found in query response.",
+        { status: 500 }
+      ); // nextStepIdがDB上で記録されているのにもかかわらず、レスポンスにはレコードが含まれていない
+    }
+
+    sortedRoutes.push(nextStep);
+    currentStep = nextStep;
+  }
+  if (currentStep.nextStepId) {
+    // これ、count>=lengthで判定してるから、これは発生しない？要検証
+    return new Response("Data error: Route step chain is circular.", { status: 500 });
+    // nextStepIdがループしている
+  }
+
+  return Nextresponse.json(sortedRoutes);
+}
