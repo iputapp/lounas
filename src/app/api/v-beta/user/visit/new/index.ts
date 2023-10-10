4d2d16951f72a45a0a890eb4e83cc6d13ae0c838
nafell <pixel68.43@gmail.com>
2023-10-11 00:40:02 +0900

    API Route: VisitHistory register POST function

--- src/app/api/v-beta/user/visit/new/index.ts
+++ src/app/api/v-beta/user/visit/new/index.ts
@@ -2,8 +2,11 @@ import { z } from "zod";
 
 export const visitRegisterRequestSchema = z.object({
   userId: z.string().uuid(),
-  dishId: z.string(),
-  date: z.date(),
+  dishId: z
+    .string()
+    .regex(/^[A-Z0-9]+$/)
+    .length(8),
+  date: z.date().optional(),
 });
 
 export type VisitRegisterRequest = z.infer<typeof visitRegisterRequestSchema>;
