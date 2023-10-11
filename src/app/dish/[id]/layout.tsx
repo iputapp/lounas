df28fc33dcf620f5128e0736bd532815380a7b2e
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-12 01:19:00 +0900

    SSG: Add layout to `dish/[id]` for gen-url

---
+++ src/app/dish/[id]/layout.tsx
@@ -0,0 +1,29 @@
+import type { Dish } from "@/app/api/v-beta/dish/[id]";
+import type { Dishes } from "@/app/api/v-beta/dishes";
+
+/** @see{@link https://nextjs.org/docs/app/api-reference/functions/generate-metadata} */
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const dish = (await fetch(`${process.env.BASE_URL}/api/v-beta/dish/${params.id}`).then((res) =>
+    res.json()
+  )) as Dish;
+  return {
+    title: `${dish.name} & ${dish.restaurant.name}`,
+  };
+}
+
+/** @see{@link https://nextjs.org/docs/app/api-reference/functions/generate-static-params} */
+export async function generateStaticParams() {
+  const dishes = (await fetch(`${process.env.BASE_URL}/api/v-beta/dishes`).then((res) =>
+    res.json()
+  )) as Dishes;
+
+  return dishes.map((dish) => ({
+    params: {
+      id: dish.id,
+    },
+  }));
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
