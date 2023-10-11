02b79e7c83232a1605fb60e97e9834ae64b9aa68
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-11 17:45:16 +0900

    Prisma-Client: Add SQL logger

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const primsaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({ log: ["query", "error", "info", "warn"] });
 };
 
 type PrismaClientSingleton = ReturnType<typeof primsaClientSingleton>;
@@ -13,10 +13,12 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? primsaClientSingleton();
+const prisma = globalForPrisma.prisma ?? primsaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
+export { prisma };
+export * from "@prisma/client";
