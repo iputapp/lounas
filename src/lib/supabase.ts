ba5da4c1137eea33de2d29be217053c224714cc1
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-12 03:43:01 +0900

    Supabase: Delete `createClient`

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,5 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { createClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
-
-/** @see {@link https://supabase.com/docs/reference/javascript/initializing} */
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
 
 export async function UserAuth() {
   const supabase = createRouteHandlerClient({ cookies });
