825f777d968db477fd6184cfc0f9cf6644d720a7
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-13 00:27:02 +0900

    API Route: Cleanup codes

--- src/app/api/v-beta/user/visits-count/route.ts
+++ src/app/api/v-beta/user/visits-count/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { userAuth } from "@/lib/supabase";
 
 import { visitsCountSchema } from ".";
