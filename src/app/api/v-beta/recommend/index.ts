3382a6085cb296d26a6e88ac4dc308ec59efd5e5
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-13 04:35:57 +0900

    API Route: Update recommend result fetching method
    - TODO: response zod validation

--- src/app/api/v-beta/recommend/index.ts
+++ src/app/api/v-beta/recommend/index.ts
@@ -1,15 +1,6 @@
 import { z } from "zod";
 
-import {
-  DishSchema,
-  DishScoreSchema,
-  DishTraitSchema,
-  PaymentSchema,
-  PaymentTypeSchema,
-  RestaurantOpenSchema,
-  RestaurantSchema,
-  WeekTypeSchema,
-} from "@/lib/zod";
+import { DishSchema, PaymentSchema, PaymentTypeSchema, RestaurantSchema } from "@/lib/zod";
 
 export const recommendRequestSchema = z.object({
   amount: z.union([z.literal("small"), z.literal("medium"), z.literal("large"), z.literal("any")]),
@@ -19,47 +10,18 @@ export const recommendRequestSchema = z.object({
 
 export type RecommendRequest = z.infer<typeof recommendRequestSchema>;
 
-// export const recommendResponseSchema = DishScoreSchema.merge(
-//   z.object({
-//     restaurant: RestaurantSchema.merge(
-//       z.object({
-//         restaurantOpens: RestaurantOpenSchema.merge(
-//           z.object({
-//             weekType: WeekTypeSchema,
-//           })
-//         ).array(),
-//         payments: PaymentSchema.merge(
-//           z.object({
-//             paymentType: PaymentTypeSchema,
-//           })
-//       ).array()
-//       }),
-//     ),
-//   })
-// ).array();
-
-export const recommendResponseSchema = DishScoreSchema.merge(
+export const recommendResponseSchema = DishSchema.merge(
   z.object({
-    dish: DishSchema.merge(
+    restaurant: RestaurantSchema.merge(
       z.object({
-        restaurant: RestaurantSchema.merge(
+        payments: PaymentSchema.merge(
           z.object({
-            restaurantOpens: RestaurantOpenSchema.merge(
-              z.object({
-                weekType: WeekTypeSchema,
-              })
-            ).array(),
-            payments: PaymentSchema.merge(
-              z.object({
-                paymentType: PaymentTypeSchema,
-              })
-            ).array(),
+            paymentType: PaymentTypeSchema,
           })
-        ),
-        dishTraits: DishTraitSchema,
+        ).array(),
       })
     ),
   })
-).array();
+);
 
 export type RecommendResponse = z.infer<typeof recommendResponseSchema>;
--- src/app/api/v-beta/recommend/route.ts
+++ src/app/api/v-beta/recommend/route.ts
@@ -1,94 +1,157 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { userAuth } from "@/lib/supabase";
 
 import { RecommendRequest, recommendRequestSchema } from ".";
 
-export async function POST(request: NextRequest) {
-  //user auth
+export async function GET(request: NextRequest) {
   const session = await userAuth();
   if (session instanceof Response) return session;
 
-  //validate request body
-  const body = (await request.json()) as Promise<RecommendRequest>;
-  const payload = recommendRequestSchema.safeParse(body);
+  const searchParams = request.nextUrl.searchParams;
+  const params = {
+    amount: searchParams.get("amount"),
+    price: Number(searchParams.get("price")),
+    commonality: searchParams.get("commonality"),
+  };
+  const payload = recommendRequestSchema.safeParse(params);
   if (!payload.success) return NextResponse.error();
 
-  const now: Date = payload.data.clientDate;
-  const dayOfWeek = now.getDay();
-  const priceMax: number = payload.data.priceMax == 0 ? 1000000 : payload.data.priceMax;
-  const lunchBreakSeconds = 45 * 60; //future: depends on organization
+  /** 今 */
+  const now = new Date();
+  /** 今日の曜日 (index) */
+  const dayOfWeek = new Date().getDay();
+  /** 時間 */
+  // const time =
+  /** 料理の値段 */
+  const price = payload.data.price;
 
-  const traits: DishScoreWhereInput[] = [];
+  /** 料理特性スコア */
+  type TraitType = keyof RecommendRequest;
+  type TraitScore = {
+    type: TraitType;
+    score: {
+      min: number;
+      max: number;
+    };
+  };
 
-  //要検討：amount scoreの範囲
-  const amountId = "？？？？？？？？？？？？？？？？？？";
-  if (payload.data.amount == "small") {
-    traits.push({
-      traitId: amountId,
-      score: {
-        lte: 30,
-      },
-    });
-  } else if (payload.data.amount == "medium") {
-    traits.push({
-      traitId: amountId,
-      score: {
-        gt: 30,
-        lte: 60,
-      },
-    });
-  } else if (payload.data.amount == "large") {
-    traits.push({
-      traitId: amountId,
-      score: {
-        gt: 60,
-      },
-    });
+  /** 絞り込み項目 */
+  const traitFilter = [] as TraitScore[];
+
+  /** @todo 要検討：amount scoreの範囲 */
+  const amountName = "amount" as TraitType;
+  switch (payload.data.amount) {
+    case "small":
+      traitFilter.push({
+        type: amountName,
+        score: {
+          min: 0,
+          max: 50,
+        },
+      });
+      break;
+    case "medium":
+      traitFilter.push({
+        type: amountName,
+        score: {
+          min: 50,
+          max: 75,
+        },
+      });
+      break;
+    case "large":
+      traitFilter.push({
+        type: amountName,
+        score: {
+          min: 75,
+          max: 100,
+        },
+      });
+      break;
+    default:
+      traitFilter.push({
+        type: amountName,
+        score: {
+          min: 0,
+          max: 100,
+        },
+      });
+      break;
   }
 
-  const commonalityId = "？？？？？？？？？？？？？？？？？？？？";
-  if (payload.data.commonality == "common") {
-    traits.push({
-      traitId: commonalityId,
-      score: {
-        lte: 60,
-      },
-    });
-  } else if (payload.data.commonality == "unique") {
-    traits.push({
-      traitId: commonalityId,
-      score: {
-        gte: 40,
-      },
-    });
+  /** @todo 要検討：commonalit scoreの範囲 */
+  const commonalityName = "commonality" as TraitType;
+  switch (payload.data.commonality) {
+    case "common":
+      traitFilter.push({
+        type: commonalityName,
+        score: {
+          min: 0,
+          max: 45,
+        },
+      });
+      break;
+    case "unique":
+      traitFilter.push({
+        type: commonalityName,
+        score: {
+          min: 45,
+          max: 100,
+        },
+      });
+      break;
+    default:
+      traitFilter.push({
+        type: commonalityName,
+        score: {
+          min: 0,
+          max: 100,
+        },
+      });
+      break;
   }
 
-  const dishes = await prisma.dish.findMany({
+  /** query result */
+  const results = await prisma.dish.findMany({
     where: {
-      //リクエストされた曜日の開店状況で絞り込む
       restaurant: {
         restaurantOpens: {
           some: {
             weekTypeId: dayOfWeek,
+            /**
+             * now BETWEEN timeOpen AND timeClose
+             * 1970-01-01 00:00:00+00 (Unix system time zero) [timestamp with time zone]
+             * @see {@link https://www.postgresql.org/docs/current/datatype-datetime.html#DATATYPE-DATETIME-SPECIAL-VALUES}
+             */
             timeOpen: {
-              lte: now,
+              lte: new Date(1970, 0, 1, now.getHours(), now.getMinutes(), now.getSeconds()),
             },
             timeClose: {
-              gte: now,
+              gte: new Date(1970, 0, 1, now.getHours(), now.getMinutes(), now.getSeconds()),
             },
           },
         },
       },
       price: {
-        lte: priceMax,
+        lte: price,
       },
-      dishScores: {
-        some: {
-          AND: traits,
-        },
-      },
+      AND: [
+        ...traitFilter.map((trait) => ({
+          dishScores: {
+            some: {
+              score: {
+                gte: trait.score.min,
+                lt: trait.score.max,
+              },
+              trait: {
+                name: trait.type,
+              },
+            },
+          },
+        })),
+      ],
     },
     include: {
       restaurant: {
@@ -103,19 +166,38 @@ export async function POST(request: NextRequest) {
     },
   });
 
-  // 45分で言って帰ってこれるお店
-  const logisticallyFeasibleDishes = dishes.filter((dish) => {
-    return dish.eatTime + dish.restaurant.travelTime * 2 <= lunchBreakSeconds;
-  });
+  /**
+   * query result zod validation
+   * @todo zod validation
+   */
+  // const items = recommendResponseSchema.parse(results);
+  const items = results;
 
+  /**
+   * お昼休みの時間
+   * @todo future: depends on organization
+   */
+  const lunchBreakTime = 45 * 60; // 45 minutes
+
+  /** お昼休みの時間でいける範囲の料理 */
+  const logisticallyFeasibleDishes = items.filter(
+    (item) => item.restaurant.travelTime * 2 + item.eatTime <= lunchBreakTime
+  );
+
+  /** 条件に当てはまる料理が5つ以上 */
   if (logisticallyFeasibleDishes.length >= 5) {
-    return NextResponse.json(logisticallyFeasibleDishes);
+    /** ランダムシャッフル + 初めから5個 */
+    const recommends = shuffleDishes(logisticallyFeasibleDishes).slice(0, 5);
+    return NextResponse.json(recommends);
+  } else {
+    /** ランダムシャッフル + 初めから5個 */
+    const recommends = shuffleDishes(items).slice(0, 5);
+    return NextResponse.json(recommends);
   }
-  //結果の中からランダムに5つ選ぶ
-  const randomDishes = logisticallyFeasibleDishes
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 5);
-
-  //return
-  return NextResponse.json(randomDishes);
 }
+
+/**
+ * shuffle dishes
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const shuffleDishes = (dishes: any[]): any[] => dishes.sort(() => Math.random() - Math.random());
