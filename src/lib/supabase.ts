7b6cf21944024441b47eb36a9263bfaf27c1ab7d
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-12 14:15:17 +0900

    Supabase: Make userAuth function error more strict

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,7 +4,8 @@ import { cookies } from "next/headers";
 export async function userAuth() {
   const supabase = createRouteHandlerClient({ cookies });
   const session = await supabase.auth.getSession();
-  if (!session.data.session) return new Response("Unauthorized", { status: 401 });
+
+  if (!session.data.session || session.error) return new Response("Unauthorized", { status: 401 });
 
   return session.data.session;
 }
