8f8ea6d762b9f18fa6cae28b50be48442705163b
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-15 03:26:25 +0900

    Revert "DB: urlid generator"
    This reverts commit cd7aedd73a888b8ef9209e50c3abffca9e360b79.

--- src/constants.ts
+++ src/constants.ts
@@ -8,10 +8,4 @@ const IPUT_STUDENT_DOMAIN = "tks.iput.ac.jp";
 /** allowed domains */
 const ALLOWED_DOMAINS = [IPUT_STUDENT_DOMAIN, "iput.ac.jp"] as readonly string[];
 
-/** length of urlId */
-const URLID_LENGTH = 8;
-
-/** characters of urlId */
-const URLID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-export { ALLOWED_DOMAINS, IPUT_STUDENT_DOMAIN, URLID_CHARS, URLID_LENGTH };
+export { ALLOWED_DOMAINS, IPUT_STUDENT_DOMAIN };
