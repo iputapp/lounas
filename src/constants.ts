cd7aedd73a888b8ef9209e50c3abffca9e360b79
nafell <pixel68.43@gmail.com>
2023-10-14 17:22:15 +0900

    DB: urlid generator

--- src/constants.ts
+++ src/constants.ts
@@ -8,4 +8,10 @@ const IPUT_STUDENT_DOMAIN = "tks.iput.ac.jp";
 /** allowed domains */
 const ALLOWED_DOMAINS = [IPUT_STUDENT_DOMAIN, "iput.ac.jp"] as readonly string[];
 
-export { ALLOWED_DOMAINS, IPUT_STUDENT_DOMAIN };
+/** length of urlId */
+const URLID_LENGTH = 8;
+
+/** characters of urlId */
+const URLID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+export { ALLOWED_DOMAINS, IPUT_STUDENT_DOMAIN, URLID_CHARS, URLID_LENGTH };
