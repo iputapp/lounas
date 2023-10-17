89deb102833a68c5eb084fb2771d54df9c1fafe4
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-17 13:18:10 +0900

    [Fix] Recommend cache settings (#201)

--- src/app/recommend/result/page.tsx
+++ src/app/recommend/result/page.tsx
@@ -11,7 +11,8 @@ import styles from "./page.module.scss";
 
 /** @see {@link https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config} */
 export const dynamic = "force-dynamic"; // SSR
-export const revalidate = 10; // 10 seconds, ガチャ対策
+export const revalidate = 0; // revalidate every request
+export const fetchCache = "force-no-store"; // no-store
 
 async function getRecommend(params: URLSearchParams) {
   const recommends = (await fetch(
