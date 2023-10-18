ddbf977e3724d665675cda48ea1663e6fdac8d22
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-18 10:52:06 +0900

    Add alert dialog before sign-out

--- src/app/webapp/user/signout/page.tsx
+++ src/app/webapp/user/signout/page.tsx
@@ -7,6 +7,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import { DialogAlert } from "@/components/dialogs/DialogAlert";
+
 import { messages, settings } from "./constants";
 import styles from "./page.module.scss";
 
@@ -14,6 +16,7 @@ export default function Page() {
   const supabase = createClientComponentClient();
   const router = useRouter();
   const [messageToUser, setMessageToUser] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   /** ガチャ */
   const messageGacha = () => {
@@ -32,7 +35,9 @@ export default function Page() {
     setMessageToUser(message);
   }, []);
 
+  /** dialog alert yes */
   const handleSignOut = async () => {
+    /** sign-out */
     await supabase.auth
       .signOut()
       .then((res) => {
@@ -40,54 +45,74 @@ export default function Page() {
           console.error("Error!", res.error.status);
           throw new Error(res.error.message);
         }
-
+        /** open dialog alert */
+        setIsOpen(false);
         router.replace("signin");
       })
       .catch((err) => console.error("Error!", err));
   };
 
+  /** dialog alert no */
+  const handleNo = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>アカウント</h1>
-      <div className={styles.sub}>
-        <span className={styles.text}>{messageToUser}</span>
-        <div className={styles.button}>
-          {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-          <button onClick={handleSignOut}>Sign Out</button>
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>アカウント</h1>
+        <div className={styles.sub}>
+          <span className={styles.text}>{messageToUser}</span>
+          <div className={styles.button}>
+            {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+            <button onClick={() => setIsOpen(true)}>Sign Out</button>
+          </div>
         </div>
-      </div>
-      <div className={styles.panel}>
-        <article>
-          {settings.map((item, index) =>
-            item.isInternal ? (
-              <Link key={index} href={item.url}>
-                <div>
-                  <span>{item.icon}</span>
-                  <span>{item.title}</span>
-                </div>
-                <span>
-                  <NavArrowRight />
-                </span>
-              </Link>
-            ) : (
-              <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">
-                <div>
-                  <span>{item.icon}</span>
-                  <span>{item.title}</span>
-                </div>
-                <div>
-                  <span>
-                    <LinkIcon />
-                  </span>
+        <div className={styles.panel}>
+          <article>
+            {settings.map((item, index) =>
+              item.isInternal ? (
+                <Link key={index} href={item.url}>
+                  <div>
+                    <span>{item.icon}</span>
+                    <span>{item.title}</span>
+                  </div>
                   <span>
                     <NavArrowRight />
                   </span>
-                </div>
-              </a>
-            )
-          )}
-        </article>
+                </Link>
+              ) : (
+                <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">
+                  <div>
+                    <span>{item.icon}</span>
+                    <span>{item.title}</span>
+                  </div>
+                  <div>
+                    <span>
+                      <LinkIcon />
+                    </span>
+                    <span>
+                      <NavArrowRight />
+                    </span>
+                  </div>
+                </a>
+              )
+            )}
+          </article>
+        </div>
       </div>
-    </div>
+      <DialogAlert
+        title="サインアウトしますか？"
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onClickYes={handleSignOut}
+        onClickNo={handleNo}
+      >
+        <div className="grid gap-1.5 text-center text-sm font-semibold">
+          <p>※再度確認コードのログインが必要になります。</p>
+        </div>
+      </DialogAlert>
+    </>
   );
 }
