995fcd1f35eafba124a42cd07595c26a44d96f7c
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-18 13:32:58 +0900

    Add disabled prop to rect-button

--- src/components/buttons/RectButton/index.tsx
+++ src/components/buttons/RectButton/index.tsx
@@ -8,15 +8,24 @@ type RectButtonProps = {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   value?: React.ButtonHTMLAttributes<HTMLButtonElement>["value"];
   autoFocus?: boolean;
+  disabled?: boolean;
 };
 
-export function RectButton({ children, color, onClick, value, autoFocus }: RectButtonProps) {
+export function RectButton({
+  children,
+  color,
+  onClick,
+  value,
+  autoFocus,
+  disabled,
+}: RectButtonProps) {
   return (
     <button
       className={`${styles.box} ${styles[color]}`}
       onClick={onClick}
       value={value}
       autoFocus={autoFocus}
+      disabled={disabled}
     >
       <span className={styles.text}>{children}</span>
     </button>
