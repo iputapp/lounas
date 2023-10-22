b9939814937be93f14ceeb915f6d4c88e256abdc
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-23 01:47:53 +0900

    Cleanup meta

--- src/app/dish/[id]/layout.tsx
+++ src/app/dish/[id]/layout.tsx
@@ -7,30 +7,48 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
   const dish = (await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/v-beta/dish/${params.id}`
   ).then((res) => res.json())) as Dish;
+
+  /** 値段 */
+  const dishPrice = dish.price.toLocaleString();
+  /** 所要時間 */
+  const requiredTime = dish.eatTime + dish.restaurant.travelTime * 2;
+  /** description */
+  const description = `The lunch recommendation app for IPUT students.\n\n料理『${dish.name}』\nお店『約${dish.restaurant.name}』\n値段『${dishPrice}円』\n所要時間『約${requiredTime}分』`;
+  /** openGraph image URL */
+  const ogImageUrl = new URL(
+    `dishes/id/${dish.id}.jpg`,
+    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1697559133`
+  );
+
   return {
     title: `${dish.name} @${dish.restaurant.name}`,
-    description: `The lunch recommendation app for IPUT students.\n\n料理『${
-      dish.name
-    }』\nお店『約${dish.restaurant.name}』\n値段『${dish.price.toLocaleString()}円』\n所要時間『${
-      dish.eatTime + dish.restaurant.travelTime * 2
-    }分』`,
+    description: description,
     openGraph: {
       title: `${dish.name} @${dish.restaurant.name} | lounas`,
-      description: `The lunch recommendation app for IPUT students.\n\n料理『${
-        dish.name
-      }』\nお店『${dish.restaurant.name}』\n値段『${dish.price.toLocaleString()}円』\n所要時間『約${
-        dish.eatTime + dish.restaurant.travelTime * 2
-      }分』`,
+      description: description,
       type: "website",
       locale: "ja_JP",
       url: new URL(`dish/${dish.id}`, process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"),
       siteName: "lounas",
       images: [
         {
-          url: new URL(
-            `dishes/id/${dish.id}.jpg`,
-            `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1697559133`
-          ),
+          type: "image/jpeg",
+          url: ogImageUrl,
+          width: 1024,
+          height: 768,
+          alt: `${dish.name} @${dish.restaurant.name}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@lounas_app",
+      title: `${dish.name} @${dish.restaurant.name} | lounas`,
+      description: description,
+      images: [
+        {
+          type: "image/jpeg",
+          url: ogImageUrl,
           width: 1024,
           height: 768,
           alt: `${dish.name} @${dish.restaurant.name}`,
--- src/app/restaurant/[id]/layout.tsx
+++ src/app/restaurant/[id]/layout.tsx
@@ -7,14 +7,17 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
   const restaurant = (await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/v-beta/restaurant/${params.id}`
   ).then((res) => res.json())) as Restaurant;
+
+  /** description */
+  const description =
+    "The lunch recommendation app for IPUT students.\n\nログインユーザーのみ閲覧可能なページです。";
+
   return {
     title: restaurant.name,
-    description:
-      "The lunch recommendation app for IPUT students.\n\nログインユーザーのみ閲覧可能なページです。",
+    description: description,
     openGraph: {
       title: `${restaurant.name} | lounas`,
-      description:
-        "The lunch recommendation app for IPUT students.\n\nログインユーザーのみ閲覧可能なページです。",
+      description: description,
       type: "website",
       locale: "ja_JP",
       url: new URL(
