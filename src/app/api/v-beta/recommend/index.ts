17401751dbd9f8d12ee3294dd3fa75e8d6d9da25
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-23 03:11:36 +0900

    [Fix] Time comparison (temporary) [preview] (#220)
    * For debug commit
    
    * For debug commit
    
    * Fix time comparison
    
    * Fix time comparison
    
    * Add sort to payment
    
    * Fix application error
    
    * Bump packages
    
    * Delete `Pisma.sql`
    
    * Add console.log
    
    * Add `Prisma.sql`
    
    * Delete `console.log`
    
    * Update meta
    
    * Add openGraph meta
    
    * Update meta
    
    * Update meta
    
    * Add OG-image
    
    * Cleanup og-image
    
    * Delete opengraph-image.tsx
    
    * Add og:image with metadata
    
    * Add top layout og:image and twitter metadata
    
    * Cleanup meta
    
    * Fix typo
    
    * Change twitter card large-image to summary

--- src/app/api/v-beta/recommend/index.ts
+++ src/app/api/v-beta/recommend/index.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+import { PaymentType } from "@/components/lists/PaymentShort";
 import {
   DishSchema,
   PaymentSchema,
@@ -32,3 +33,63 @@ export const recommendResponseSchema = DishSchema.merge(
 );
 
 export type RecommendResponse = z.infer<typeof recommendResponseSchema>;
+
+/** Temporary workaround until Prisma supports JST */
+/**
+ * @todo Remove this when Prisma supports JST
+ */
+type RecommendQuery = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  eat_time: number;
+  restaurant_id: string;
+  restaurant_name: string;
+  restaurant_description: string;
+  website: string;
+  address: string;
+  longitude: number;
+  latitude: number;
+  travel_time: number;
+  travel_distance: number;
+  time_open: string;
+  time_close: string;
+  payment_name: string;
+  payment_accepted: boolean;
+  payment_details: string;
+};
+
+/**
+ * @todo Remove this when Prisma supports JST
+ */
+type RecommendFixed = {
+  basic: Omit<RecommendQuery, "payment_name" | "payment_accepted" | "payment_details">;
+  payments: {
+    type: PaymentType;
+    accepted: boolean;
+  }[];
+};
+
+/**
+ * @todo Remove this when Prisma supports JST
+ */
+type RecommendResponseF = {
+  dish: {
+    id: string;
+    name: string;
+    price: number;
+    eatTime: number;
+  };
+  restaurant: {
+    id: string;
+    name: string;
+    travelTime: number;
+    travelDistance: number;
+    time_open: string;
+    time_close: string;
+    payments: Pick<RecommendFixed, "payments">["payments"];
+  };
+};
+
+export type { PaymentType, RecommendFixed, RecommendQuery, RecommendResponseF };
--- src/app/restaurant/[id]/layout.tsx
+++ src/app/restaurant/[id]/layout.tsx
@@ -7,9 +7,25 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
   const restaurant = (await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/v-beta/restaurant/${params.id}`
   ).then((res) => res.json())) as Restaurant;
+
+  /** description */
+  const description =
+    "The lunch recommendation app for IPUT students.\n\nログインユーザーのみ閲覧可能なページです。";
+
   return {
     title: restaurant.name,
-    description: "ログインユーザーのみ閲覧可能なページです。",
+    description: description,
+    openGraph: {
+      title: `${restaurant.name} | lounas`,
+      description: description,
+      type: "website",
+      locale: "ja_JP",
+      url: new URL(
+        `restaurant/${restaurant.id}`,
+        process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+      ),
+      siteName: "lounas",
+    },
   } as Metadata;
 }
 
