87d760a662fbda8fcfd3b80c2315e381c8abd5b0
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-31 09:12:59 +0900

    Add double-check-method to `webapp/user` page
    - Force revalidate per an access
    - Redirect correct path with session

--- src/app/webapp/user/page.ts
+++ src/app/webapp/user/page.ts
@@ -1,9 +1,23 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { redirect, RedirectType } from "next/navigation";
 
 /**
  * redirects users to the signout page
- * access control is handled by the middleware
+ * access control is handled by the middleware (double check)
  */
-export default function Page() {
-  return redirect("/webapp/user/signout", RedirectType.replace);
+export default async function Page() {
+  const cookieStore = cookies();
+
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const session = await supabase.auth.getSession();
+
+  revalidatePath("/webapp/user");
+
+  if (session.data.session) {
+    return redirect("/webapp/user/signout", RedirectType.replace);
+  }
+
+  return redirect("/webapp/user/signin", RedirectType.replace);
 }
