b3f3bae04091b2e2ceb8275b2ce97249a366266f
wiyco <72733890+wiyco@users.noreply.github.com>
2023-10-31 12:12:42 +0900

    Update to correct text of `webapp/user` page

--- src/app/webapp/user/signin/page.tsx
+++ src/app/webapp/user/signin/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
     <div className={styles.container}>
       <h1 className={styles.title}>アカウント</h1>
       <div className={styles.sub}>
-        <span className={styles.text}>IMAP会員へようこそ！</span>
+        <span className={styles.text}>lounasへようこそ！</span>
         <div className={styles.button}>
           <BorderRoundButton fontSize="text-2xl" onClick={handleSignIn}>
             Sign In
@@ -35,11 +35,15 @@ export default function Page() {
               <p>このアプリについて</p>
             </section>
             <section>
-              <p>私たちは、多様なバックグラウンドや経験を持つ個々の個人から成るグループです。</p>
               <p>
+                {"　"}私たちは、多様なバックグラウンドや経験を持つ個々の個人から成るグループです。
+              </p>
+              <p>
+                {"　"}
                 共に協力し合い、様々な問題を解決していく中で、私たちの成長と共に、より良い未来を築くことを目標としています。
               </p>
               <p>
+                {"　"}
                 互いに尊重し、協力し、理解し合うことを大切にし、さまざまな視点からアイデアを活用していきます。
               </p>
             </section>
