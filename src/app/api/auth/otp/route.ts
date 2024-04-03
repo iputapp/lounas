ab0301b722827839177d32c52e5b8fa483750e66
nafell <pixel68.43@gmail.com>
2024-04-03 15:42:52 +0900

    fix: cookie store email value

--- src/app/api/auth/otp/route.ts
+++ src/app/api/auth/otp/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
   /** store email to cookie */
   cookieStore.set({
     name: "email",
-    value: email,
+    value: payload.data.email,
     path: "/",
     sameSite: "strict",
     secure: true,
