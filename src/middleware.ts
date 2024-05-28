1dcc5aa898cd15c188604c459a1869cd4d00df9e
wiyco <me@mail.wiyco.dev>
2024-05-29 02:29:49 +0900

    refactor: supabase ssr related

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,128 +1,186 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import type { NextRequest } from "next/server";
-import { NextResponse, userAgent } from "next/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse, userAgent } from "next/server";
 
 /** @see {@link https://nextjs.org/docs/app/building-your-application/routing/middleware} */
-export async function middleware(req: NextRequest) {
-  const reqUrl = new URL(req.url);
-  const reqNextPath = req.nextUrl.pathname;
-  /** supabase auth session */
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  const session = await supabase.auth.getSession();
+export async function middleware(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const requestPath = request.nextUrl.pathname;
 
-  /** session token expired */
-  if (session.error && !reqNextPath.startsWith("/signup")) {
-    console.warn("session", session.error);
-    return NextResponse.redirect(`${reqUrl.origin}/signup`);
-  }
+  /** @see {@link https://supabase.com/docs/guides/auth/server-side/creating-a-client?environment=middleware&queryGroups=framework&framework=nextjs&queryGroups=environment} */
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  /** supabase client */
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
+
+  /** get supabase auth user */
+  const auth = await supabase.auth.getUser();
 
   /** @see {@link https://nextjs.org/docs/messages/middleware-parse-user-agent} */
-  const { device } = userAgent(req);
+  const { device } = userAgent(request);
   console.log("device", device);
 
   /** signined */
-  if (session.data.session) {
+  if (auth.data.user && !auth.error) {
     /** log */
-    console.log("session", {
-      email: session.data.session.user.email,
-      expiredIn: session.data.session.expires_in,
+    console.log("auth", {
+      email: auth.data.user.email,
+      expiredIn: (await supabase.auth.getSession()).data.session?.expires_in,
     });
 
     /** root */
-    if (reqNextPath === "/") {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp`);
+    if (requestPath === "/") {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp`);
     }
 
     /** ----- auth ----- */
     /** /signup */
-    if (reqNextPath.startsWith("/signup")) {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp`);
+    if (requestPath.startsWith("/signup")) {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp`);
     }
 
     /** ----- app ----- */
     /** /webapp */
-    if (reqNextPath.endsWith("/webapp")) {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp/home`);
+    if (requestPath.endsWith("/webapp")) {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp/home`);
     }
 
     /** /webapp/user */
-    if (reqNextPath.endsWith("/signin")) {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp/user/signout`);
+    if (requestPath.endsWith("/signin")) {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp/user/signout`);
+    }
+
+    /** /recommend */
+    if (requestPath.endsWith("/recommend")) {
+      return NextResponse.redirect(`${requestUrl.origin}/recommend/explore`);
     }
 
     /** refreshing session */
-    return res;
+    return response;
   } else {
     /** ----- root ----- */
-    if (reqNextPath === "/") {
+    if (requestPath === "/") {
       if (device.type === "mobile") {
-        return NextResponse.rewrite(`${reqUrl.origin}/mobile`);
+        return NextResponse.rewrite(`${requestUrl.origin}/mobile`);
       }
     }
 
     /** top page (mobile) */
-    if (reqNextPath.endsWith("/mobile")) {
+    if (requestPath.endsWith("/mobile")) {
       if (device.type !== "mobile") {
-        return NextResponse.redirect(`${reqUrl.origin}`);
+        return NextResponse.redirect(`${requestUrl.origin}`);
       }
     }
 
     /** ----- auth ----- */
     /** /signup/verify */
-    if (reqNextPath.endsWith("/verify")) {
+    if (requestPath.endsWith("/verify")) {
       /** pending verification */
-      if (!req.cookies.has("verify")) return NextResponse.redirect(`${reqUrl.origin}`);
+      if (!request.cookies.has("verify")) return NextResponse.redirect(`${requestUrl.origin}`);
 
       /** remove access permissions to the verify page */
-      res.cookies.delete("verify");
-      return res;
+      response.cookies.delete("verify");
+      return response;
     }
 
     /** ----- settings ----- */
     /** /privacy */
-    if (reqNextPath.startsWith("/privacy")) {
-      return NextResponse.redirect(`${reqUrl.origin}`);
+    if (requestPath.startsWith("/privacy")) {
+      return NextResponse.redirect(`${requestUrl.origin}`);
     }
 
     /** ----- app ----- */
     /** /webapp */
-    if (reqNextPath.endsWith("/webapp")) {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp/user`);
+    if (requestPath.endsWith("/webapp")) {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp/user`);
     }
 
     /** /webapp/home */
-    if (reqNextPath.endsWith("/home")) {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp/user`);
+    if (requestPath.endsWith("/home")) {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp/user`);
     }
 
     /** /webapp/user */
-    if (reqNextPath.endsWith("/signout")) {
-      return NextResponse.redirect(`${reqUrl.origin}/webapp/user/signin`);
+    if (requestPath.endsWith("/signout")) {
+      return NextResponse.redirect(`${requestUrl.origin}/webapp/user/signin`);
     }
 
     /** /recommend */
-    if (reqNextPath.startsWith("/recommend")) {
-      return NextResponse.redirect(`${reqUrl.origin}`);
+    if (requestPath.startsWith("/recommend")) {
+      return NextResponse.redirect(`${requestUrl.origin}`);
     }
 
     /** ----- data ----- */
     /** /restaurant */
-    if (reqNextPath.startsWith("/restaurant")) {
-      return NextResponse.redirect(`${reqUrl.origin}`);
+    if (requestPath.startsWith("/restaurant")) {
+      return NextResponse.redirect(`${requestUrl.origin}`);
     }
   }
 }
 
 export const config = {
   matcher: [
-    "/",
-    "/mobile/:path*",
-    "/signup/:path+",
-    "/privacy/:path*",
-    "/dish/:path*",
-    "/restaurant/:path*",
-    "/webapp/:path*",
-    "/recommend/:path*",
+    // "/",
+    // "/mobile/:path*",
+    // "/signup/:path+",
+    // "/privacy/:path*",
+    // "/dish/:path*",
+    // "/restaurant/:path*",
+    // "/webapp/:path*",
+    // "/recommend/:path*",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
