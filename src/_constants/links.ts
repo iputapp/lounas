2d82c0b4badb5bda5adf02621d2b9ef54781bc76
wiyco <me@mail.wiyco.dev>
2024-05-30 19:23:14 +0900

    refactor: add media links enum

---
+++ src/_constants/links.ts
@@ -0,0 +1,9 @@
+enum MediaLinks {
+  FEEDBACK_FORM = "https://forms.gle/c1gosjcJZf5gTtvCA",
+  IPUTAPP = "https://iputapp.lounas.jp",
+  X = "https://x.com/iputapp",
+  INSTAGRAM = "https://www.instagram.com/iputapp",
+  GITHUB = "https://github.com/iputapp/lounas",
+}
+
+export { MediaLinks };
